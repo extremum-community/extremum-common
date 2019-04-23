a7c68b8b50471a76a2a6fe720e316640ba56c140
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-04-23 16:25:48 +0400

    CS-147 Data layer for storing collections
    - apply collections makeup via controller aspect

---
+++ src/main/java/com/extremum/common/collection/conversion/ResponseCollectionsMakeupAdvice.java
@@ -0,0 +1,47 @@
+package com.extremum.common.collection.conversion;
+
+import com.extremum.common.dto.CommonResponseDto;
+import com.extremum.common.response.Response;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @author rpuch
+ */
+@ControllerAdvice
+public class ResponseCollectionsMakeupAdvice implements ResponseBodyAdvice<Response> {
+    private final CollectionMakeup makeup;
+
+    public ResponseCollectionsMakeupAdvice(CollectionMakeup makeup) {
+        this.makeup = makeup;
+    }
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return returnType.getParameterType() == Response.class;
+    }
+
+    @Override
+    public Response beforeBodyWrite(Response body, MethodParameter returnType, MediaType selectedContentType,
+            Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request,
+            ServerHttpResponse response) {
+        applyIfNeeded(body);
+        return body;
+    }
+
+    private void applyIfNeeded(Response response) {
+        if (response == null) {
+            return;
+        }
+        if (!(response.getResult() instanceof CommonResponseDto)) {
+            return;
+        }
+
+        makeup.applyCollectionMakeup((CommonResponseDto) response.getResult());
+    }
+}
---
+++ src/test/java/com/extremum/common/collection/conversion/ResponseCollectionsMakeupAdviceTest.java
@@ -0,0 +1,113 @@
+package com.extremum.common.collection.conversion;
+
+import com.extremum.common.dto.CommonResponseDto;
+import com.extremum.common.response.Response;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author rpuch
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ResponseCollectionsMakeupAdviceTest {
+    @InjectMocks
+    private ResponseCollectionsMakeupAdvice advice;
+
+    @Mock
+    private CollectionMakeup makeup;
+
+    @Mock
+    private ServerHttpRequest request;
+    @Mock
+    private ServerHttpResponse response;
+
+    @Test
+    public void givenReturnedValueIsResponse_whenSupportsIsCalled_thenShouldReturnTrue() {
+        boolean supports = advice.supports(returnType(ReturnsResponse.class), null);
+        assertThat(supports, is(true));
+    }
+
+    @Test
+    public void givenReturnedValueIsNotResponse_whenSupportsIsCalled_thenShouldReturnFalse() {
+        boolean supports = advice.supports(returnType(ReturnsString.class), null);
+        assertThat(supports, is(false));
+    }
+
+    @Test
+    public void givenReturnedValueIsResponseWithDto_whenBeforeBedyWriteIsCalled_thenMakeupIsApplied() {
+        TestResponseDto dto = new TestResponseDto();
+        Response responseValue = Response.ok(dto);
+
+        advice.beforeBodyWrite(responseValue, returnType(ReturnsResponse.class), MediaType.APPLICATION_JSON,
+                null, request, response);
+
+        verify(makeup).applyCollectionMakeup(dto);
+    }
+
+    @Test
+    public void givenReturnedValueIsResponseWithNonDto_whenBeforeBedyWriteIsCalled_thenMakeupIsNotApplied() {
+        Response responseValue = Response.ok("test");
+
+        advice.beforeBodyWrite(responseValue, returnType(ReturnsResponse.class), MediaType.APPLICATION_JSON,
+                null, request, response);
+
+        verify(makeup, never()).applyCollectionMakeup(any());
+    }
+
+    @Test
+    public void givenReturnedValueIsNull_whenBeforeBedyWriteIsCalled_thenMakeupIsNotApplied() {
+        Response responseValue = null;
+
+        advice.beforeBodyWrite(responseValue, returnType(ReturnsResponse.class), MediaType.APPLICATION_JSON,
+                null, request, response);
+
+        verify(makeup, never()).applyCollectionMakeup(any());
+    }
+
+    @Test
+    public void givenReturnedValueIsResponseWithNullResult_whenBeforeBedyWriteIsCalled_thenMakeupIsNotApplied() {
+        Response responseValue = Response.ok(null);
+
+        advice.beforeBodyWrite(responseValue, returnType(ReturnsResponse.class), MediaType.APPLICATION_JSON,
+                null, request, response);
+
+        verify(makeup, never()).applyCollectionMakeup(any());
+    }
+
+    private MethodParameter returnType(Class<?> handlerClass) {
+        try {
+            return new MethodParameter(handlerClass.getMethod("handle"), -1);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static class ReturnsResponse {
+        public Response handle() {
+            return null;
+        }
+    }
+
+    private static class ReturnsString {
+        public String handle() {
+            return "test";
+        }
+    }
+
+    private static class TestResponseDto extends CommonResponseDto {
+
+    }
+}
