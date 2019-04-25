9372295bfa131e509ce16a3bd7681edf6ddd3209
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-04-25 10:54:35 +0400

    CS-147 Data layer for storing collections
    - add accessors to 'deleted' field

--- src/main/java/com/extremum/common/collection/CollectionDescriptor.java
+++ src/main/java/com/extremum/common/collection/CollectionDescriptor.java
@@ -42,6 +42,14 @@ public final class CollectionDescriptor implements Serializable {
                 new CollectionCoordinates(new EmbeddedCoordinates(hostId, hostFieldName)));
     }
 
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
     @Override
     public String toString() {
         return externalId;
