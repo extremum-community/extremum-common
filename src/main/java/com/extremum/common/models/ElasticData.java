ef79c91b0d0e57f21281196704e854abb63c5d12
Kirill Jonua <k.djonua@gmail.com>
2019-04-26 10:18:57 +0300

    ElasticData remove duplicated fields definition

--- src/main/java/com/extremum/common/models/ElasticData.java
+++ src/main/java/com/extremum/common/models/ElasticData.java
@@ -1,7 +1,6 @@
 package com.extremum.common.models;
 
 import com.extremum.common.descriptor.Descriptor;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,21 +11,29 @@ import java.time.ZonedDateTime;
 
 @Getter
 @Setter
-@Builder
 @ToString
 @NoArgsConstructor
-@AllArgsConstructor
 public class ElasticData extends ElasticCommonModel {
     private String rawDocument;
-    private Descriptor uuid;
-    private String id;
-    private ZonedDateTime created;
-    private ZonedDateTime modified;
-    private Long version;
-    private Boolean deleted;
     private String modelName;
     private Long seqNo;
     private Long primaryTerm;
+
+    @Builder
+    public ElasticData(Descriptor uuid, String id, ZonedDateTime created, ZonedDateTime modified, Long version,
+                       Boolean deleted, String rawDocument, String modelName, Long seqNo, Long primaryTerm) {
+        setUuid(uuid);
+        setId(id);
+        setCreated(created);
+        setModified(modified);
+        setVersion(version);
+        setDeleted(deleted);
+
+        this.rawDocument = rawDocument;
+        this.modelName = modelName;
+        this.seqNo = seqNo;
+        this.primaryTerm = primaryTerm;
+    }
 
     @Override
     public String getModelName() {
