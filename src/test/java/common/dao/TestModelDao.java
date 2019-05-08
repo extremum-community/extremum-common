44c62a3f5e6c77ad647660ef957947be8919d16a
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-05-08 18:18:02 +0400

    CS-172 Migrate from morphia to spring data
    - add SpringDataMongoCommonDao

--- src/test/java/common/dao/TestModelDao.java
+++ src/test/java/common/dao/TestModelDao.java
@@ -1,11 +1,9 @@
 package common.dao;
 
-import com.extremum.common.dao.MongoCommonDao;
+import com.extremum.common.dao.impl.SpringDataMongoCommonDao;
 import models.TestModel;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TestModelDao extends MongoCommonDao<TestModel>, MongoRepository<TestModel, ObjectId> {
+public interface TestModelDao extends SpringDataMongoCommonDao<TestModel> {
 }
