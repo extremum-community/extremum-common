7b020676d724e8e92833bf6e780bd50a9ab2ddb4
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-05-17 20:00:52 +0400

    CS-189 Make sure that UUID is generated without a round-trip to the database
    - generate JPA entities IDs using Hibernate's custom version 4 UUID generator that uses IP address instead of MAC address

--- src/main/java/com/extremum/common/models/PostgresCommonModel.java
+++ src/main/java/com/extremum/common/models/PostgresCommonModel.java
@@ -4,6 +4,8 @@ import com.extremum.common.descriptor.Descriptor;
 import com.extremum.common.service.lifecycle.JpaCommonModelLifecycleListener;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -19,18 +21,22 @@ public abstract class PostgresCommonModel implements PersistableCommonModel<UUID
     @Getter(onMethod_ = {@Transient})
     private Descriptor uuid;
 
-    @Getter(onMethod_ = {@Id, @GeneratedValue})
     private UUID id;
-
     @Getter(onMethod_ = {@CreatedDate})
     private ZonedDateTime created;
-
     @Getter(onMethod_ = {@LastModifiedDate})
     private ZonedDateTime modified;
-
     @Getter(onMethod_ = {@Version})
     private Long version;
-
     @Getter(onMethod_ = {@Transient})
     private Boolean deleted = false;
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator", parameters = {
+            @Parameter(name = "uuid_gen_strategy_class", value = "org.hibernate.id.uuid.CustomVersionOneStrategy")
+    })
+    public UUID getId() {
+        return id;
+    }
 }
