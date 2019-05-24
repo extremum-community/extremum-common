e65d859394d00c776e21c3691616f4815a7ad561
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-05-24 19:10:12 +0400

    CS-202 An attempt to get a non-existent record via everything-everything should not cause 500 error
    - make that sure EverythingEverythingConfiguration is auto-configured before WebMvcAutoConfiguration so that the controller advice gets picked up on time

--- src/main/java/com/extremum/everything/config/EverythingEverythingConfiguration.java
+++ src/main/java/com/extremum/everything/config/EverythingEverythingConfiguration.java
@@ -32,9 +32,11 @@ import com.extremum.everything.services.management.EverythingEverythingManagemen
 import com.extremum.starter.CommonConfiguration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -49,6 +51,7 @@ import java.util.List;
 @EnableConfigurationProperties({DestroyerProperties.class, ModelProperties.class})
 @Import(DefaultServicesConfiguration.class)
 @AutoConfigureAfter(CommonConfiguration.class)
+@AutoConfigureBefore(WebMvcAutoConfiguration.class)
 public class EverythingEverythingConfiguration {
     private final ModelProperties modelProperties;
     private final DestroyerProperties destroyerProperties;
