a44882671188d450685e70b5c1c0caafd743a3be
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-06-19 23:50:34 +0400

    CS-221 Migrate Elastic support to spring-data-elastic
    - move common methods to BaseElasticsearchRepository

--- extremum-elasticsearch-starter/src/main/java/com/extremum/elasticsearch/repositories/BaseElasticsearchRepository.java
+++ extremum-elasticsearch-starter/src/main/java/com/extremum/elasticsearch/repositories/BaseElasticsearchRepository.java
@@ -1,14 +1,27 @@
 package com.extremum.elasticsearch.repositories;
 
+import com.extremum.common.utils.DateUtils;
 import com.extremum.common.utils.StreamUtils;
 import com.extremum.elasticsearch.dao.ElasticsearchCommonDao;
 import com.extremum.elasticsearch.model.ElasticsearchCommonModel;
+import org.elasticsearch.action.DocWriteResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.script.Script;
+import org.elasticsearch.script.ScriptType;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.query.UpdateQuery;
+import org.springframework.data.elasticsearch.core.query.UpdateQueryBuilder;
 import org.springframework.data.elasticsearch.repository.support.ElasticsearchEntityInformation;
 import org.springframework.data.elasticsearch.repository.support.SimpleElasticsearchRepository;
 
+import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -16,24 +29,32 @@ import java.util.stream.Collectors;
  */
 abstract class BaseElasticsearchRepository<T extends ElasticsearchCommonModel>
         extends SimpleElasticsearchRepository<T, String> implements ElasticsearchCommonDao<T> {
-    protected BaseElasticsearchRepository(ElasticsearchEntityInformation<T, String> metadata,
+    private static final String PAINLESS_LANGUAGE = "painless";
+
+    private static final String MODIFIED = ElasticsearchCommonModel.FIELDS.modified.name();
+
+    private final ElasticsearchEntityInformation<T, String> metadata;
+
+    BaseElasticsearchRepository(ElasticsearchEntityInformation<T, String> metadata,
             ElasticsearchOperations elasticsearchOperations) {
         super(metadata, elasticsearchOperations);
+
+        this.metadata = metadata;
     }
 
     @Override
-    public List<T> findAll() {
+    public final List<T> findAll() {
         throw new UnsupportedOperationException("" +
                 "Please do not call this method as a list of all documents may be very large");
     }
 
     @Override
-    public Iterable<T> findAll(Sort sort) {
+    public final Iterable<T> findAll(Sort sort) {
         throw new UnsupportedOperationException("" +
                 "Please do not call this method as a list of all documents may be very large");
     }
 
-    final <S> List<S> iterableToList(Iterable<S> iterable) {
+    private <S> List<S> iterableToList(Iterable<S> iterable) {
         return StreamUtils.fromIterable(iterable).collect(Collectors.toList());
     }
 
@@ -43,9 +64,60 @@ abstract class BaseElasticsearchRepository<T extends ElasticsearchCommonModel>
         return iterableToList(savedEntities);
     }
 
+    @Override
+    public final void deleteAll() {
+        throw new UnsupportedOperationException("We don't allow to delete all the documents in one go");
+    }
 
     @Override
-    public void deleteAll() {
-        throw new UnsupportedOperationException("We don't allow to delete all the records in one go");
+    public List<T> search(String queryString) {
+        Iterable<T> results = search(QueryBuilders.queryStringQuery(queryString));
+        return iterableToList(results);
+    }
+
+    @Override
+    public boolean patch(String id, String painlessScript) {
+        return patch(id, painlessScript, Collections.emptyMap());
+    }
+
+    @Override
+    public boolean patch(String id, String painlessScript, Map<String, Object> scriptParams) {
+        UpdateRequest updateRequest = new UpdateRequest(metadata.getIndexName(), id);
+        Script script = createScript(painlessScript, scriptParams);
+        updateRequest.script(script);
+
+        UpdateQuery updateQuery = new UpdateQueryBuilder()
+                .withClass(metadata.getJavaType())
+                .withId(id)
+                .withUpdateRequest(updateRequest)
+                .build();
+
+        UpdateResponse response = elasticsearchOperations.update(updateQuery);
+        return response.getResult() == DocWriteResponse.Result.UPDATED;
+    }
+
+    private Script createScript(String painlessScript, Map<String, Object> params) {
+        String scriptWithModificationTimeChange = amendWithModificationTimeChange(painlessScript);
+        Map<String, Object> paramsWithModificationTimeChange = amendWithModificationTime(params);
+        return new Script(ScriptType.INLINE, PAINLESS_LANGUAGE, scriptWithModificationTimeChange,
+                paramsWithModificationTimeChange);
+    }
+
+    private String amendWithModificationTimeChange(String painlessScript) {
+        return painlessScript + changeModificationTimeScriptSuffix();
+    }
+
+    private String changeModificationTimeScriptSuffix() {
+        return "; ctx._source." + MODIFIED + " = params." + MODIFIED;
+    }
+
+    private Map<String, Object> amendWithModificationTime(Map<String, Object> params) {
+        Map<String, Object> paramsWithModificationTimeChange = new HashMap<>(params);
+        paramsWithModificationTimeChange.put(MODIFIED, getNowAsString());
+        return paramsWithModificationTimeChange;
+    }
+
+    private String getNowAsString() {
+        return DateUtils.formatZonedDateTimeISO_8601(ZonedDateTime.now());
     }
 }
--- extremum-elasticsearch-starter/src/main/java/com/extremum/elasticsearch/repositories/SoftDeleteElasticsearchRepository.java
+++ extremum-elasticsearch-starter/src/main/java/com/extremum/elasticsearch/repositories/SoftDeleteElasticsearchRepository.java
@@ -1,36 +1,24 @@
 package com.extremum.elasticsearch.repositories;
 
 import com.extremum.common.models.PersistableCommonModel;
-import com.extremum.common.utils.DateUtils;
 import com.extremum.common.utils.StreamUtils;
 import com.extremum.elasticsearch.model.ElasticsearchCommonModel;
-import org.elasticsearch.action.DocWriteResponse;
-import org.elasticsearch.action.update.UpdateRequest;
-import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.script.Script;
-import org.elasticsearch.script.ScriptType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.query.*;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.data.elasticsearch.repository.support.ElasticsearchEntityInformation;
 
-import java.time.ZonedDateTime;
-import java.util.*;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
  * @author rpuch
  */
 public class SoftDeleteElasticsearchRepository<T extends ElasticsearchCommonModel> extends BaseElasticsearchRepository<T> {
-    private static final String PAINLESS_LANGUAGE = "painless";
-
-    private static final String MODIFIED = ElasticsearchCommonModel.FIELDS.modified.name();
-
     private final ElasticsearchOperations elasticsearchOperations;
-    private final ElasticsearchEntityInformation<T, String> metadata;
 
     private final SoftDeletion softDeletion = new SoftDeletion();
 
@@ -40,13 +28,6 @@ public class SoftDeleteElasticsearchRepository<T extends ElasticsearchCommonMode
         super(metadata, elasticsearchOperations);
 
         this.elasticsearchOperations = elasticsearchOperations;
-        this.metadata = metadata;
-    }
-
-    @Override
-    public List<T> search(String queryString) {
-        Iterable<T> results = search(QueryBuilders.queryStringQuery(queryString));
-        return iterableToList(results);
     }
 
     @Override
@@ -67,59 +48,8 @@ public class SoftDeleteElasticsearchRepository<T extends ElasticsearchCommonMode
     }
 
     @Override
-    public boolean patch(String id, String painlessScript) {
-        return patch(id, painlessScript, Collections.emptyMap());
-    }
-
-    @Override
-    public boolean patch(String id, String painlessScript, Map<String, Object> scriptParams) {
-        UpdateRequest updateRequest = new UpdateRequest(metadata.getIndexName(), id);
-        Script script = createScript(painlessScript, scriptParams);
-        updateRequest.script(script);
-
-        UpdateQuery updateQuery = new UpdateQueryBuilder()
-                .withClass(metadata.getJavaType())
-                .withId(id)
-                .withUpdateRequest(updateRequest)
-                .build();
-
-        UpdateResponse response = elasticsearchOperations.update(updateQuery);
-        return response.getResult() == DocWriteResponse.Result.UPDATED;
-    }
-
-    private Script createScript(String painlessScript, Map<String, Object> params) {
-        String scriptWithModificationTimeChange = amendWithModificationTimeChange(painlessScript);
-        Map<String, Object> paramsWithModificationTimeChange = amendWithModificationTime(params);
-        return new Script(ScriptType.INLINE, PAINLESS_LANGUAGE, scriptWithModificationTimeChange,
-                paramsWithModificationTimeChange);
-    }
-
-    private String amendWithModificationTimeChange(String painlessScript) {
-        return painlessScript + changeModificationTimeScriptSuffix();
-    }
-
-    private String changeModificationTimeScriptSuffix() {
-        return "; ctx._source." + MODIFIED + " = params." + MODIFIED;
-    }
-
-    private Map<String, Object> amendWithModificationTime(Map<String, Object> params) {
-        Map<String, Object> paramsWithModificationTimeChange = new HashMap<>(params);
-        paramsWithModificationTimeChange.put(MODIFIED, getNowAsString());
-        return paramsWithModificationTimeChange;
-    }
-
-    private String getNowAsString() {
-        return DateUtils.formatZonedDateTimeISO_8601(ZonedDateTime.now());
-    }
-
-    @Override
     public void deleteById(String id) {
         patch(id, "ctx._source.deleted = true");
-    }
-
-    @Override
-    public final void deleteAll() {
-        throw new UnsupportedOperationException("We don't allow to delete all the documents in one go");
     }
 
     @Override
