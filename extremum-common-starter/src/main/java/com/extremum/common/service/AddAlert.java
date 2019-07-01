ea0404aff5010a265178aaa842a568198351c358
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-07-01 14:15:52 +0400

    CS-257 Replace Collection<Alert> parameter with Problems parameter in CommonService
    - check that alerts collection is not null

--- extremum-common-starter/src/main/java/com/extremum/common/service/AddAlert.java
+++ extremum-common-starter/src/main/java/com/extremum/common/service/AddAlert.java
@@ -2,16 +2,21 @@ package com.extremum.common.service;
 
 import com.extremum.common.exceptions.CommonException;
 import com.extremum.common.response.Alert;
-import lombok.RequiredArgsConstructor;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * @author rpuch
  */
-@RequiredArgsConstructor
 public final class AddAlert implements Problems {
     private final Collection<Alert> alerts;
+
+    public AddAlert(Collection<Alert> alerts) {
+        Objects.requireNonNull(alerts, "Alerts collection cannot be null");
+
+        this.alerts = alerts;
+    }
 
     @Override
     public void accept(CommonException e) {
