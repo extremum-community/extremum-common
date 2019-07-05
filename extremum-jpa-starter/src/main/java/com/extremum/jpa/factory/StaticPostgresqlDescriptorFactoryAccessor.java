645aa00e47ff97f31e85be9aeb1d5ff63efd1f75
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-07-05 14:24:42 +0400

    CS-321 Reduce static usage of DescriptorService
    - rename a field

--- extremum-jpa-starter/src/main/java/com/extremum/jpa/factory/StaticPostgresqlDescriptorFactoryAccessor.java
+++ extremum-jpa-starter/src/main/java/com/extremum/jpa/factory/StaticPostgresqlDescriptorFactoryAccessor.java
@@ -4,13 +4,13 @@ package com.extremum.jpa.factory;
  * @author rpuch
  */
 public class StaticPostgresqlDescriptorFactoryAccessor {
-    private static volatile PostgresqlDescriptorFacilities FACTORY_INSTANCE;
+    private static volatile PostgresqlDescriptorFacilities FACILITIES_INSTANCE;
 
     public static PostgresqlDescriptorFacilities getFactory() {
-        return FACTORY_INSTANCE;
+        return FACILITIES_INSTANCE;
     }
 
     public static void setFactory(PostgresqlDescriptorFacilities factory) {
-        FACTORY_INSTANCE = factory;
+        FACILITIES_INSTANCE = factory;
     }
 }
