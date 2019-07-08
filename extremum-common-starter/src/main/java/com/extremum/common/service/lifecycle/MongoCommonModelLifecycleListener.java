94fc4d84e2506b4f5049ca3635ee05c678422143
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-07-08 17:57:40 +0400

    CS-265 Always create Descriptor before storing an entity in a database
    - generate internalId and descriptor in the case of Mongo before saving the entity to Mongo

--- extremum-common-starter/src/main/java/com/extremum/common/service/lifecycle/MongoCommonModelLifecycleListener.java
+++ extremum-common-starter/src/main/java/com/extremum/common/service/lifecycle/MongoCommonModelLifecycleListener.java
@@ -3,6 +3,7 @@ package com.extremum.common.service.lifecycle;
 import com.extremum.common.descriptor.factory.MongoDescriptorFacilities;
 import com.extremum.common.models.MongoCommonModel;
 import com.extremum.common.utils.ModelUtils;
+import com.extremum.sharedmodels.descriptor.Descriptor;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
 import org.springframework.data.mongodb.core.mapping.event.AfterConvertEvent;
 import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
@@ -29,6 +30,11 @@ public class MongoCommonModelLifecycleListener extends AbstractMongoEventListene
     
     private void fillRequiredFields(MongoCommonModel model) {
         if (model.getId() == null && model.getUuid() != null) {
+            model.setId(mongoDescriptorFacilities.resolve(model.getUuid()));
+        } else if (model.getUuid() == null) {
+            String modelName = ModelUtils.getModelName(model);
+            Descriptor descriptor = mongoDescriptorFacilities.createWithNewInternalId(modelName);
+            model.setUuid(descriptor);
             model.setId(mongoDescriptorFacilities.resolve(model.getUuid()));
         }
     }
