038fd37f55e6a50bacfed71b7c17fcb17cef17fe
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-07-08 17:02:43 +0400

    CS-329 Create extremum module for shared models
    - use @JsonProperty

--- extremum-shared-models/src/main/java/com/extremum/sharedmodels/basic/Status.java
+++ extremum-shared-models/src/main/java/com/extremum/sharedmodels/basic/Status.java
@@ -1,32 +1,12 @@
 package com.extremum.sharedmodels.basic;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum Status {
-    draft("draft"), active("active"), hidden("hidden");
-
-    private final String value;
-
-    Status(String value) {
-        this.value = value;
-    }
-
-    @JsonValue
-    public String getValue() {
-        return value;
-    }
-
-    @JsonCreator
-    public static Status fromString(String value) {
-        if (value != null) {
-            for (Status type : Status.values()) {
-                if (value.equalsIgnoreCase(type.value)) {
-                    return type;
-                }
-            }
-        }
-
-        return null;
-    }
+    @JsonProperty
+    draft,
+    @JsonProperty
+    active,
+    @JsonProperty
+    hidden;
 }
