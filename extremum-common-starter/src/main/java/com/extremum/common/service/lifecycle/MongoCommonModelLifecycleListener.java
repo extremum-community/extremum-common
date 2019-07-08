e929b22667d41a4f3e6269e487ed7e19a75b1a28
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-07-08 19:25:15 +0400

    CS-265 Always create Descriptor before storing an entity in a database
    - make code more readable

--- extremum-common-starter/src/main/java/com/extremum/common/service/lifecycle/MongoCommonModelLifecycleListener.java
+++ extremum-common-starter/src/main/java/com/extremum/common/service/lifecycle/MongoCommonModelLifecycleListener.java
@@ -4,6 +4,7 @@ import com.extremum.common.descriptor.factory.MongoDescriptorFacilities;
 import com.extremum.common.models.MongoCommonModel;
 import com.extremum.common.utils.ModelUtils;
 import com.extremum.sharedmodels.descriptor.Descriptor;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
 import org.springframework.data.mongodb.core.mapping.event.AfterConvertEvent;
 import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
@@ -29,19 +30,28 @@ public final class MongoCommonModelLifecycleListener extends AbstractMongoEventL
     }
     
     private void fillRequiredFields(MongoCommonModel model) {
-        if (model.getId() == null && model.getUuid() != null) {
-            model.setId(mongoDescriptorFacilities.resolve(model.getUuid()));
-        } else if (model.getUuid() == null) {
-            String modelName = ModelUtils.getModelName(model);
-            if (model.getId() == null) {
-                Descriptor descriptor = mongoDescriptorFacilities.createWithNewInternalId(modelName);
-                model.setUuid(descriptor);
-                model.setId(mongoDescriptorFacilities.resolve(model.getUuid()));
-            } else {
-                Descriptor descriptor = mongoDescriptorFacilities.create(model.getId(), modelName);
-                model.setUuid(descriptor);
-            }
+        final boolean internalIdGiven = model.getId() != null;
+        final boolean uuidGiven = model.getUuid() != null;
+
+        if (uuidGiven && !internalIdGiven) {
+            model.setId(getInternalIdFromDescriptor(model));
+        } else if (!uuidGiven && internalIdGiven) {
+            Descriptor descriptor = createAndSaveDescriptorWithGivenInternalId(model.getId(), model);
+            model.setUuid(descriptor);
+        } else if (!uuidGiven && !internalIdGiven) {
+            Descriptor descriptor = createAndSaveDescriptorWithGivenInternalId(new ObjectId(), model);
+            model.setUuid(descriptor);
+            model.setId(getInternalIdFromDescriptor(model));
         }
+    }
+
+    private ObjectId getInternalIdFromDescriptor(MongoCommonModel model) {
+        return mongoDescriptorFacilities.resolve(model.getUuid());
+    }
+
+    private Descriptor createAndSaveDescriptorWithGivenInternalId(ObjectId objectId, MongoCommonModel model) {
+        String modelName = ModelUtils.getModelName(model);
+        return mongoDescriptorFacilities.create(objectId, modelName);
     }
 
     @Override
