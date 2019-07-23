2dee647838f823a7cd32de6c08c0593fbbfc5376
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-07-23 16:47:24 +0400

    CS-371 Integrate Everything-Everything security with authentication service
    - remove 'order' field

--- extremum-shared-models/src/main/java/com/extremum/sharedmodels/personal/VerifyType.java
+++ extremum-shared-models/src/main/java/com/extremum/sharedmodels/personal/VerifyType.java
@@ -1,29 +1,23 @@
 package com.extremum.sharedmodels.personal;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@RequiredArgsConstructor
-@Getter
 public enum VerifyType {
     @JsonProperty("email")
-    EMAIL(0),
+    EMAIL,
     @JsonProperty("sms")
-    SMS(0),
+    SMS,
     @JsonProperty("email.verify")
-    EMAIL_VERIFY(1),
+    EMAIL_VERIFY,
     @JsonProperty("sms.verify")
-    SMS_VERIFY(1),
+    SMS_VERIFY,
     @JsonProperty("username")
-    USERNAME(-1),
+    USERNAME,
     @JsonProperty("password")
-    PASSWORD(-1);
-
-    private final int order;
+    PASSWORD;
 
     boolean inSameGroupWith(VerifyType otherType) {
         Set<VerifyType> thisAndThat = new HashSet<>();
