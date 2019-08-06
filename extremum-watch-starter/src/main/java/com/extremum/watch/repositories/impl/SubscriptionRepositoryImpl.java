f74008b71fbd992a60b32def3e6968a50228eae3
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-08-06 17:56:08 +0400

    CS-448 Rework Redis watched events storage
    - use Redis SET to store watch subscriptions

--- extremum-watch-starter/src/main/java/com/extremum/watch/repositories/impl/SubscriptionRepositoryImpl.java
+++ extremum-watch-starter/src/main/java/com/extremum/watch/repositories/impl/SubscriptionRepositoryImpl.java
@@ -2,65 +2,55 @@ package com.extremum.watch.repositories.impl;
 
 import com.extremum.watch.config.SubscriptionProperties;
 import com.extremum.watch.repositories.SubscriptionRepository;
-import org.redisson.api.MapOptions;
-import org.redisson.api.RMapCache;
+import org.redisson.api.RSetCache;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 @Repository
 public class SubscriptionRepositoryImpl implements SubscriptionRepository {
     private final RedissonClient client;
     private final SubscriptionProperties properties;
 
-    private RMapCache<String, String> subscriptionsMap;
-    private static final String SUBSCRIPTIONS = "watch_subscriptions_idx";
-    private static final String DELIMITER = "/";
-
     public SubscriptionRepositoryImpl(RedissonClient client, SubscriptionProperties properties) {
         this.client = client;
         this.properties = properties;
     }
 
-    @PostConstruct
-    private void init() {
-        subscriptionsMap = client.getMapCache(SUBSCRIPTIONS, MapOptions.defaults());
-    }
-
-    private void save(String modelId, String subscriberId) {
-        subscriptionsMap.put(getKeyPattern(subscriberId, modelId), "",
-                properties.getTimeToLive(), TimeUnit.DAYS,
-                properties.getIdleTime(), TimeUnit.DAYS);
-    }
-
     @Override
     public void subscribe(Collection<String> modelIds, String subscriberId) {
-        modelIds.forEach(modelId -> save(modelId, subscriberId));
+        modelIds.forEach(modelId -> subscribeToOne(modelId, subscriberId));
     }
 
-    private void remove(String modelId, String subscriberId) {
-        subscriptionsMap.remove(getKeyPattern(subscriberId, modelId));
+    private void subscribeToOne(String modelId, String subscriberId) {
+        RSetCache<String> subscribers = subscriptionSet(modelId);
+        subscribers.add(subscriberId, properties.getTimeToLive(), TimeUnit.DAYS);
+    }
+
+    private RSetCache<String> subscriptionSet(String modelId) {
+        return client.getSetCache(subscriptionKey(modelId));
+    }
+
+    private String subscriptionKey(String modelId) {
+        return "watch-subscription:" + modelId;
     }
 
     @Override
     public void unsubscribe(Collection<String> modelIds, String subscriberId) {
-        modelIds.forEach(modelId -> remove(modelId, subscriberId));
+        modelIds.forEach(modelId -> unsubscribeFromOne(modelId, subscriberId));
+    }
+
+    private void unsubscribeFromOne(String modelId, String subscriberId) {
+        RSetCache<String> subscribers = subscriptionSet(modelId);
+        subscribers.remove(subscriberId);
     }
 
     @Override
     public Collection<String> getAllSubscribersIdsBySubscription(String modelId) {
-//        TODO refactor keySet() method to native script for Redis 
-        return subscriptionsMap.keySet(getKeyPattern("*", modelId))
-                .stream()
-                .map(key -> key.split(DELIMITER)[0])
-                .collect(Collectors.toList());
+        return new ArrayList<>(subscriptionSet(modelId));
     }
 
-    private String getKeyPattern(String subscriberId, String modelId) {
-        return String.join(DELIMITER, subscriberId, modelId);
-    }
 }
--- extremum-watch-starter/src/test/java/com/extremum/watch/repositories/SubscriptionRepositoryTest.java
+++ extremum-watch-starter/src/test/java/com/extremum/watch/repositories/SubscriptionRepositoryTest.java
@@ -2,16 +2,19 @@ package com.extremum.watch.repositories;
 
 import com.extremum.watch.config.TestWithServices;
 import com.extremum.watch.config.WatchTestConfiguration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
 @SpringBootTest(classes = WatchTestConfiguration.class)
@@ -20,14 +23,34 @@ class SubscriptionRepositoryTest extends TestWithServices {
     @Autowired
     private SubscriptionRepository subscriptionRepository;
 
-    @BeforeAll
-    void setUp() {
-        subscriptionRepository.subscribe(Collections.singletonList("12"), "test");
+    @Test
+    void givenOneSubscriptionExists_whenFindingSubscribersForThisModel_thenTheSubscriberShouldBeReturned() {
+        subscriptionRepository.subscribe(Collections.singletonList("dead"), "Alex");
+
+        Collection<String> subscribers = subscriptionRepository.getAllSubscribersIdsBySubscription("dead");
+        
+        assertThat(subscribers, equalTo(Collections.singletonList("Alex")));
     }
 
     @Test
-    void testPatternGet() {
-        Collection<String> bySubscription = subscriptionRepository.getAllSubscribersIdsBySubscription("12");
-        assertEquals(1, bySubscription.size(), "Need to find one subscriber");
+    void givenTwoSubscriptionsExistForTwoSubscribers_whenFindingSubscribersForOneModel_thenBothSubscriberShouldBeReturned() {
+        subscriptionRepository.subscribe(Arrays.asList("dead", "beef"), "Alex");
+        subscriptionRepository.subscribe(Arrays.asList("dead", "beef"), "Ben");
+
+        Collection<String> subscribers = subscriptionRepository.getAllSubscribersIdsBySubscription("dead");
+
+        assertThat(subscribers, hasSize(2));
+        assertThat(subscribers, hasItems("Alex", "Ben"));
+    }
+
+    @Test
+    void givenTwoSubscriptionsExistForTwoSubscribers_whenAlexDeletesOneSubscription_thenForItAlexShouldNotBeFound() {
+        subscriptionRepository.subscribe(Arrays.asList("dead", "beef"), "Alex");
+        subscriptionRepository.subscribe(Arrays.asList("dead", "beef"), "Ben");
+        subscriptionRepository.unsubscribe(Collections.singletonList("dead"), "Alex");
+
+        Collection<String> subscribers = subscriptionRepository.getAllSubscribersIdsBySubscription("dead");
+
+        assertThat(subscribers, equalTo(Collections.singletonList("Ben")));
     }
 }
