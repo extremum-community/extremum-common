60a90f3ae5fe21429d230fec7b5cf0f21801b101
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-08-06 14:59:54 +0400

    CS-445 Make watch GET controller adhere to the documentation
    - add tests

--- extremum-watch-starter/src/test/java/com/extremum/watch/repositories/TextWatchEventRepositoryTest.java
+++ extremum-watch-starter/src/test/java/com/extremum/watch/repositories/TextWatchEventRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.extremum.watch.repositories;
 
-import com.extremum.common.mongo.MongoConstants;
 import com.extremum.watch.config.BaseConfig;
 import com.extremum.watch.config.TestWithServices;
 import com.extremum.watch.controller.ModelWithFilledValues;
@@ -10,10 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.ZonedDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
+import static com.extremum.common.mongo.MongoConstants.DISTANT_FUTURE;
+import static com.extremum.common.mongo.MongoConstants.DISTANT_PAST;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 
@@ -26,13 +28,14 @@ class TextWatchEventRepositoryTest extends TestWithServices {
     @Autowired
     private TextWatchEventRepository repository;
 
+    private final String subscriber = randomString();
+
     @Test
     void whenSearchingBySubscriberInSubscribers_thenShouldFindSomething() {
-        String subscriber = randomString();
         saveAnEventVisibleFor(subscriber);
 
         List<TextWatchEvent> events = repository.findBySubscribersAndCreatedBetweenOrderByCreatedAscIdAsc(
-                subscriber, MongoConstants.DISTANT_PAST, MongoConstants.DISTANT_FUTURE);
+                subscriber, DISTANT_PAST, DISTANT_FUTURE);
         
         assertThat(events, hasSize(1));
     }
@@ -46,14 +49,43 @@ class TextWatchEventRepositoryTest extends TestWithServices {
 
     @Test
     void whenSearchingBySubscriberNotInSubscribers_thenShouldNotFindAnything() {
-        String subscriber = randomString();
         saveAnEventVisibleFor(subscriber);
 
         String anotherSubscriber = randomString();
         List<TextWatchEvent> events = repository.findBySubscribersAndCreatedBetweenOrderByCreatedAscIdAsc(
-                anotherSubscriber, MongoConstants.DISTANT_PAST, MongoConstants.DISTANT_FUTURE);
+                anotherSubscriber, DISTANT_PAST, DISTANT_FUTURE);
 
         assertThat(events, hasSize(0));
+    }
+
+    @Test
+    void whenSearchingUntilYesterday_thenShouldNotFoundAnything() {
+        saveAnEventVisibleFor(subscriber);
+
+        List<TextWatchEvent> events = repository.findBySubscribersAndCreatedBetweenOrderByCreatedAscIdAsc(
+                subscriber, DISTANT_PAST, yesterday());
+
+        assertThat(events, hasSize(0));
+    }
+
+    @NotNull
+    private ZonedDateTime yesterday() {
+        return ZonedDateTime.now().minusDays(1);
+    }
+
+    @Test
+    void whenSearchingFromTomorrow_thenShouldNotFoundAnything() {
+        saveAnEventVisibleFor(subscriber);
+
+        List<TextWatchEvent> events = repository.findBySubscribersAndCreatedBetweenOrderByCreatedAscIdAsc(
+                subscriber, tomorrow(), DISTANT_FUTURE);
+
+        assertThat(events, hasSize(0));
+    }
+
+    @NotNull
+    private ZonedDateTime tomorrow() {
+        return ZonedDateTime.now().plusDays(1);
     }
 
     @NotNull
