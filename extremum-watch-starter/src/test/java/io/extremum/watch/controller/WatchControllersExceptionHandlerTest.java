adff2fd412a47413ad384088a958e925be9eca9d
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-08-12 21:25:12 +0400

    CS-450 Only allow users with enough privileges to watch models
    - handle ExtremumAccessDeniedException

---
+++ extremum-watch-starter/src/test/java/io/extremum/watch/controller/WatchControllersExceptionHandlerTest.java
@@ -0,0 +1,81 @@
+package io.extremum.watch.controller;
+
+import io.extremum.common.response.Response;
+import io.extremum.everything.controllers.EverythingExceptionHandlerTarget;
+import io.extremum.security.ExtremumAccessDeniedException;
+import io.extremum.sharedmodels.dto.RequestDto;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author rpuch
+ */
+class WatchControllersExceptionHandlerTest {
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                 .standaloneSetup(new TestController())
+                 .setControllerAdvice(new WatchControllersExceptionHandler())
+                 .build();
+    }
+
+    @Test
+    void whenExtremumAccessDeniedExceptionIsThrown_thenProper403ResponseShouldBeReturnedAsResponseMessageCodeAttribute()
+            throws Exception {
+        JSONObject root = getSuccessfullyAndParseResponse("/extremum-access-denied-exception");
+
+        assertThat(root.getString("status"), is("FAIL"));
+        assertThat(root.getInt("code"), is(403));
+        assertThat(root.getString("result"), is(nullValue()));
+    }
+
+    @NotNull
+    private JSONObject getSuccessfullyAndParseResponse(String uri) throws Exception {
+        MvcResult result = mockMvc.perform(get(uri))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+        return parseResponse(result);
+    }
+
+    @NotNull
+    private JSONObject parseResponse(MvcResult result) throws UnsupportedEncodingException, JSONException {
+        String content = result.getResponse().getContentAsString();
+        return new JSONObject(content);
+    }
+
+    @RestController
+    @EverythingExceptionHandlerTarget
+    private static class TestController {
+        @RequestMapping("/ok")
+        Response ok() {
+            return Response.ok("Success!");
+        }
+
+        @RequestMapping("/extremum-access-denied-exception")
+        Response extremumAccessDeniedException() {
+            throw new ExtremumAccessDeniedException("Access denied");
+        }
+    }
+
+    private static class TestRequestDto implements RequestDto {
+
+    }
+}
