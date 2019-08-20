69db4df55531072b63568b7c237d75ec5fc8fd4a
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-08-20 15:12:36 +0400

    CS-505 Create collection streaming prototype
    - fix compilation

--- extremum-jpa-starter/src/main/java/io/extremum/jpa/services/impl/PostgresBasicServiceImpl.java
+++ extremum-jpa-starter/src/main/java/io/extremum/jpa/services/impl/PostgresBasicServiceImpl.java
@@ -1,9 +1,11 @@
 package io.extremum.jpa.services.impl;
 
+import io.extremum.common.service.Problems;
 import io.extremum.common.service.impl.CommonServiceImpl;
 import io.extremum.jpa.dao.PostgresCommonDao;
 import io.extremum.jpa.models.PostgresBasicModel;
 import io.extremum.jpa.services.PostgresBasicService;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
@@ -18,5 +20,15 @@ public class PostgresBasicServiceImpl<M extends PostgresBasicModel> extends Comm
     @Override
     protected UUID stringToId(String id) {
         return UUID.fromString(id);
+    }
+
+    @Override
+    public Mono<M> reactiveGet(String id) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public Mono<M> reactiveGet(String id, Problems problems) {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 }
