ac096e9a2458402cab134d23d4f5461b06400c93
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-08-23 17:33:46 +0400

    CS-509 Implement universal reactive loader for JPA
    - deprecate a testing-only class

--- extremum-common-starter/src/main/java/io/extremum/common/reactive/NaiveReactifier.java
+++ extremum-common-starter/src/main/java/io/extremum/common/reactive/NaiveReactifier.java
@@ -9,6 +9,7 @@ import java.util.function.Supplier;
  * A naive implementation of {@link Reactifier} suitable for tests.
  * DO NOT USE IN PRODUCTION CODE!
  */
+@Deprecated
 public class NaiveReactifier implements Reactifier {
     @Override
     public <T> Mono<T> mono(Supplier<? extends T> objectSupplier) {
