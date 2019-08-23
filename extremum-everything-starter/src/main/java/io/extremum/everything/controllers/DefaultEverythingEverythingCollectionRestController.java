98b1cf827a453b3567148a135ed6cd88dd9d2a9f
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-08-23 15:36:16 +0400

    CS-532 Handle streaming errors
    - output a special event ('internal-error') to communicate an error

--- extremum-everything-starter/src/main/java/io/extremum/everything/controllers/DefaultEverythingEverythingCollectionRestController.java
+++ extremum-everything-starter/src/main/java/io/extremum/everything/controllers/DefaultEverythingEverythingCollectionRestController.java
@@ -7,8 +7,13 @@ import io.extremum.everything.services.management.EverythingCollectionManagement
 import io.extremum.sharedmodels.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,9 +29,22 @@ public class DefaultEverythingEverythingCollectionRestController
     }
 
     @GetMapping(value = "/collection/{collectionId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public @ResponseBody
-    Flux<ResponseDto> streamCollection(@PathVariable String collectionId, Projection projection,
-                                       @RequestParam(defaultValue = "false") boolean expand) {
-        return collectionManagementService.streamCollection(collectionId, projection, expand);
+    public Flux<ServerSentEvent<Object>> streamCollection(@PathVariable String collectionId, Projection projection,
+            @RequestParam(defaultValue = "false") boolean expand) {
+        return collectionManagementService.streamCollection(collectionId, projection, expand)
+                .map(this::dtoToSse)
+                .onErrorResume(e -> Mono.just(throwableToSse(e)));
+    }
+
+    private ServerSentEvent<Object> dtoToSse(ResponseDto dto) {
+        return ServerSentEvent.builder()
+                .data(dto)
+                .build();
+    }
+
+    private ServerSentEvent<Object> throwableToSse(Throwable e) {
+        return ServerSentEvent.builder().event("internal-error")
+                .data(e.getMessage())
+                .build();
     }
 }
--- extremum-everything-starter/src/test/java/io/extremum/everything/controllers/DefaultEverythingEverythingCollectionRestControllerFailingStreamingTest.java
+++ extremum-everything-starter/src/test/java/io/extremum/everything/controllers/DefaultEverythingEverythingCollectionRestControllerFailingStreamingTest.java
@@ -1,6 +1,5 @@
 package io.extremum.everything.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.extremum.everything.services.management.EverythingCollectionManagementService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,62 +10,34 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import reactor.core.publisher.Flux;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = {EverythingControllersTestConfiguration.class,
         DefaultEverythingEverythingCollectionRestController.class})
 @AutoConfigureMockMvc
-class DefaultEverythingEverythingCollectionRestControllerTest {
+class DefaultEverythingEverythingCollectionRestControllerFailingStreamingTest {
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @MockBean
     private EverythingCollectionManagementService collectionManagementService;
 
     @Test
-    void streamsCollection() throws Exception {
+    void whenAnExceptionOccursDuringStreaming_thenItShouldBeHandled() throws Exception {
         when(collectionManagementService.streamCollection(anyString(), any(), anyBoolean()))
-                .thenReturn(Flux.just(new TestResponseDto("first"), new TestResponseDto("second")));
+                .thenReturn(Flux.error(new RuntimeException("Oops!")));
 
         String responseText = mockMvc.perform(get("/collection/dead-beef").accept(MediaType.TEXT_EVENT_STREAM))
                 .andExpect(status().is2xxSuccessful())
-                .andExpect(content().string(not(isEmptyString())))
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
 
-        assertThatFirstAndSecondAreReturned(responseText);
-    }
-
-    private void assertThatFirstAndSecondAreReturned(String responseText) {
-        List<TestResponseDto> dtos = new NaiveTextEventStream(responseText).dataStream()
-                .map(this::parseResponseDto)
-                .collect(Collectors.toList());
-        assertThat(dtos, hasSize(2));
-        assertThat(dtos.get(0).name, is("first"));
-        assertThat(dtos.get(1).name, is("second"));
-    }
-
-    private TestResponseDto parseResponseDto(String data) {
-        try {
-            return objectMapper.readerFor(TestResponseDto.class).readValue(data);
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot parse", e);
-        }
+        assertThat(responseText, is("event:internal-error\ndata:Oops!\n\n"));
     }
 }
