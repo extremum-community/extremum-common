059863723849a56d3b3b27b7e2b365e1f082912f
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-08-26 12:00:36 +0400

    CS-532 Handle streaming errors
    - rewrite streaming test from MockMvc to WebTestClient because MockMvc is not suitable for testing WebFlux applications

--- extremum-everything-starter/src/test/java/io/extremum/everything/controllers/DefaultEverythingEverythingCollectionRestControllerTest.java
+++ extremum-everything-starter/src/test/java/io/extremum/everything/controllers/DefaultEverythingEverythingCollectionRestControllerTest.java
@@ -1,88 +1,72 @@
 package io.extremum.everything.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.extremum.everything.services.management.EverythingCollectionManagementService;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = {EverythingControllersTestConfiguration.class,
-        DefaultEverythingEverythingCollectionRestController.class})
-@AutoConfigureMockMvc
+@WebFluxTest
+@ContextConfiguration(classes = EverythingControllersTestConfiguration.class)
 class DefaultEverythingEverythingCollectionRestControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
+    private WebTestClient webClient;
 
     @MockBean
     private EverythingCollectionManagementService collectionManagementService;
 
-    @Test
-    void streamsCollection() throws Exception {
-        when(collectionManagementService.streamCollection(anyString(), any(), anyBoolean()))
-                .thenReturn(Flux.just(new TestResponseDto("first"), new TestResponseDto("second")));
-
-        String responseText = mockMvc.perform(get("/collection/dead-beef").accept(MediaType.TEXT_EVENT_STREAM))
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(content().string(not(isEmptyString())))
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
-
-        assertThatFirstAndSecondAreReturned(responseText);
+    @BeforeEach
+    void initClient() {
+        Object controller = new DefaultEverythingEverythingCollectionRestController(collectionManagementService);
+        webClient = WebTestClient.bindToController(controller).build();
     }
 
-    private void assertThatFirstAndSecondAreReturned(String responseText) {
-        List<TestResponseDto> dtos = new NaiveTextEventStream(responseText).dataStream()
-                .map(this::parseResponseDto)
-                .collect(Collectors.toList());
+    @Test
+    void streamsCollection() {
+        when(collectionManagementService.streamCollection(eq("dead-beef"), any(), anyBoolean()))
+                .thenReturn(Flux.just(new TestResponseDto("first"), new TestResponseDto("second")));
+
+        List<TestResponseDto> dtos = webClient.get().uri("/collection/dead-beef")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBodyList(TestResponseDto.class)
+                .returnResult()
+                .getResponseBody();
+
+        assertThatFirstAndSecondAreReturned(dtos);
+    }
+
+    private void assertThatFirstAndSecondAreReturned(List<TestResponseDto> dtos) {
         assertThat(dtos, hasSize(2));
         assertThat(dtos.get(0).name, is("first"));
         assertThat(dtos.get(1).name, is("second"));
     }
 
-    private TestResponseDto parseResponseDto(String data) {
-        try {
-            return objectMapper.readerFor(TestResponseDto.class).readValue(data);
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot parse", e);
-        }
-    }
-
     @Test
-    @Disabled("Enable when a concurrency bug that makes one (or even both) tests in this file fail is identified and fixed")
-    void whenAnExceptionOccursDuringStreaming_thenItShouldBeHandled() throws Exception {
-        when(collectionManagementService.streamCollection(anyString(), any(), anyBoolean()))
+    void whenAnExceptionOccursDuringStreaming_thenItShouldBeHandled() {
+        when(collectionManagementService.streamCollection(eq("dead-beef"), any(), anyBoolean()))
                 .thenReturn(Flux.error(new RuntimeException("Oops!")));
 
-        String responseText = mockMvc.perform(get("/collection/dead-beef").accept(MediaType.TEXT_EVENT_STREAM))
-                .andExpect(status().is2xxSuccessful())
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
+        String responseText = webClient.get().uri("/collection/dead-beef")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(String.class)
+                .returnResult()
+                .getResponseBody();
 
         assertThat(responseText, startsWith("event:internal-error\ndata:Internal error "));
     }
