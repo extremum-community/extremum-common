f2f42c92c32f32a8ba9f496672f1a772eebdaf7f
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-09-04 21:31:10 +0400

    CS-543 Add Mongo implementation of ReactiveCommonService
    - emit after-conversion event for query methods

--- extremum-common-starter/src/main/java/io/extremum/mongo/springdata/reactiverepository/ExtremumReactiveMongoRepositoryFactory.java
+++ extremum-common-starter/src/main/java/io/extremum/mongo/springdata/reactiverepository/ExtremumReactiveMongoRepositoryFactory.java
@@ -1,13 +1,20 @@
 package io.extremum.mongo.springdata.reactiverepository;
 
+import io.extremum.common.reactive.ReactiveEventPublisher;
 import io.extremum.common.repository.SeesSoftlyDeletedRecords;
 import io.extremum.common.utils.ModelUtils;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import org.springframework.data.mongodb.core.mapping.event.AfterConvertEvent;
 import org.springframework.data.mongodb.repository.support.ReactiveMongoRepositoryFactory;
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.query.QueryLookupStrategy;
 import org.springframework.data.repository.query.QueryMethodEvaluationContextProvider;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import javax.annotation.Nullable;
 import java.util.Optional;
 
 /**
@@ -17,22 +24,32 @@ import java.util.Optional;
  * queries generated from query methods like <code>Person findByEmail(String email)</code>
  * respect the 'deleted' flag (unless annotated with &#064;{@link SeesSoftlyDeletedRecords}).
  *
+ * Also, this repository factory wraps all query method invocations to make sure
+ * that for every object returned inside a {@link Flux} or a {@link Mono},
+ * an {@link AfterConvertEvent} will be emitted reactively.
+ *
  * @author rpuch
  * @see SeesSoftlyDeletedRecords
  */
 public class ExtremumReactiveMongoRepositoryFactory extends ReactiveMongoRepositoryFactory {
     private final Class<?> repositoryInterface;
     private final ReactiveLookupStrategies lookupStrategies;
+    private final ReactiveEventPublisher reactiveEventPublisher;
 
     public ExtremumReactiveMongoRepositoryFactory(Class<?> repositoryInterface,
-                                                  ReactiveMongoOperations mongoOperations) {
+                                                  ReactiveMongoOperations mongoOperations,
+                                                  ReactiveEventPublisher reactiveEventPublisher) {
         super(mongoOperations);
         this.repositoryInterface = repositoryInterface;
         lookupStrategies = new ReactiveLookupStrategies(mongoOperations);
+        this.reactiveEventPublisher = reactiveEventPublisher;
+
+        addRepositoryProxyPostProcessor((factory, repositoryInformation)
+                -> factory.addAdvice(new QueryMethodAfterLoadEventEmitter()));
     }
 
     @Override
-    protected Optional<QueryLookupStrategy> getQueryLookupStrategy(QueryLookupStrategy.Key key,
+    protected Optional<QueryLookupStrategy> getQueryLookupStrategy(@Nullable QueryLookupStrategy.Key key,
             QueryMethodEvaluationContextProvider evaluationContextProvider) {
         if (isSoftDelete()) {
             Optional<QueryLookupStrategy> optStrategy = super.getQueryLookupStrategy(key, evaluationContextProvider);
@@ -54,5 +71,32 @@ public class ExtremumReactiveMongoRepositoryFactory extends ReactiveMongoReposit
     private boolean isSoftDelete() {
         RepositoryMetadata metadata = getRepositoryMetadata(repositoryInterface);
         return ModelUtils.isSoftDeletable(metadata.getDomainType());
+    }
+
+    private class QueryMethodAfterLoadEventEmitter implements MethodInterceptor {
+        @Override
+        @Nullable
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            Object result = invocation.proceed();
+            if (result == null) {
+                return null;
+            }
+
+            if (result instanceof Mono) {
+                Mono<?> mono = (Mono<?>) result;
+                return mono.flatMap(object -> {
+                    return reactiveEventPublisher.publishEvent(new AfterConvertEvent<>(null, object, null))
+                            .thenReturn(object);
+                });
+            }
+            if (result instanceof Flux) {
+                Flux<?> flux = (Flux<?>) result;
+                return flux.flatMap(object -> {
+                    return reactiveEventPublisher.publishEvent(new AfterConvertEvent<>(null, object, null));
+                });
+            }
+
+            return result;
+        }
     }
 }
