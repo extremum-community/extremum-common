caed0a5c29b685ba527f9a5de464f1b5533715d6
Yaroslav Paramonov <student.help.96@gmail.com>
2019-09-05 17:23:40 +0300

    add validation for credential

--- extremum-shared-models/src/main/java/io/extremum/sharedmodels/personal/Credential.java
+++ extremum-shared-models/src/main/java/io/extremum/sharedmodels/personal/Credential.java
@@ -4,6 +4,8 @@ import io.extremum.sharedmodels.annotation.DocumentationName;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -11,12 +13,15 @@ import lombok.*;
 @ToString(exclude = "value")
 @DocumentationName("Credential")
 public class Credential {
+    @NotNull
     @JsonProperty("system")
     private String system;
 
+    @NotNull
     @JsonProperty("name")
     private VerifyType type;
 
+    @NotNull
     @JsonProperty("value")
     private String value;
 }
