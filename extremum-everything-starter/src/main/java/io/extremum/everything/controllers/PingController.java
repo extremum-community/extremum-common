0e5751dffe6546b84e38afc04a097f4661115d80
Kirill Jonua <k.djonua@gmail.com>
2019-09-26 15:25:47 +0300

    CS-715 swagger integration: DefaultEverythingEverythingRestController.java & PingController.java

--- extremum-everything-starter/src/main/java/io/extremum/everything/controllers/PingController.java
+++ extremum-everything-starter/src/main/java/io/extremum/everything/controllers/PingController.java
@@ -1,13 +1,22 @@
 package io.extremum.everything.controllers;
 
 import io.extremum.sharedmodels.dto.Response;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api("Ping")
 @RestController
 public class PingController {
+    @ApiOperation(value = "Ping")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK")
+    })
     @GetMapping(value = "/ping")
-    public Response ping () {
+    public Response ping() {
         return Response.ok();
     }
 }
