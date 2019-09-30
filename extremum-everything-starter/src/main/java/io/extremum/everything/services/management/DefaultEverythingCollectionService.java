a84b714c98fd5e2fb2cb91b9311aa59405307dcc
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-09-30 10:10:08 +0400

    CS-724 Add free collections to Collections Framework
    - extract OwnedCoordinatesHandler

--- extremum-everything-starter/src/main/java/io/extremum/everything/services/management/DefaultEverythingCollectionService.java
+++ extremum-everything-starter/src/main/java/io/extremum/everything/services/management/DefaultEverythingCollectionService.java
@@ -2,41 +2,37 @@ package io.extremum.everything.services.management;
 
 import io.extremum.common.dto.converters.ConversionConfig;
 import io.extremum.common.dto.converters.services.DtoConversionService;
-import io.extremum.common.model.BasicModel;
-import io.extremum.sharedmodels.basic.Model;
 import io.extremum.common.reactive.Reactifier;
 import io.extremum.common.tx.CollectionTransactivity;
 import io.extremum.everything.collection.CollectionFragment;
 import io.extremum.everything.collection.Projection;
 import io.extremum.everything.dao.UniversalDao;
-import io.extremum.everything.exceptions.EverythingEverythingException;
 import io.extremum.everything.services.OwnedCollectionFetcher;
 import io.extremum.everything.services.OwnedCollectionStreamer;
 import io.extremum.everything.services.collection.CoordinatesHandler;
-import io.extremum.everything.services.collection.FetchByOwnedCoordinates;
-import io.extremum.everything.services.collection.StreamByOwnedCoordinates;
-import io.extremum.sharedmodels.descriptor.CollectionCoordinates;
+import io.extremum.sharedmodels.basic.Model;
 import io.extremum.sharedmodels.descriptor.CollectionDescriptor;
-import io.extremum.sharedmodels.descriptor.OwnedCoordinates;
 import io.extremum.sharedmodels.dto.ResponseDto;
-import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
-import java.util.Optional;
 
-import static java.lang.String.format;
-
-@RequiredArgsConstructor
 public class DefaultEverythingCollectionService implements EverythingCollectionService {
-    private final ModelRetriever modelRetriever;
-    private final List<OwnedCollectionFetcher> ownedCollectionFetchers;
-    private final List<OwnedCollectionStreamer> ownedCollectionStreamers;
     private final DtoConversionService dtoConversionService;
-    private final UniversalDao universalDao;
-    private final Reactifier reactifier;
-    private final CollectionTransactivity transactivity;
+
+    private final OwnedCoordinatesHandler ownedCoordinatesHandler;
+
+    public DefaultEverythingCollectionService(ModelRetriever modelRetriever,
+            List<OwnedCollectionFetcher> ownedCollectionFetchers,
+            List<OwnedCollectionStreamer> ownedCollectionStreamers,
+            DtoConversionService dtoConversionService, UniversalDao universalDao,
+            Reactifier reactifier, CollectionTransactivity transactivity) {
+        this.dtoConversionService = dtoConversionService;
+
+        this.ownedCoordinatesHandler = new OwnedCoordinatesHandler(modelRetriever, universalDao,
+                ownedCollectionFetchers, ownedCollectionStreamers, reactifier, transactivity);
+    }
 
     @Override
     public CollectionFragment<ResponseDto> fetchCollection(CollectionDescriptor id,
@@ -55,7 +51,7 @@ public class DefaultEverythingCollectionService implements EverythingCollectionS
 
     private CoordinatesHandler findCoordinatesHandler(CollectionDescriptor.Type type) {
         if (type == CollectionDescriptor.Type.OWNED) {
-            return new OwnedCoordinatesHandler();
+            return ownedCoordinatesHandler;
         }
 
         throw new IllegalStateException("Unsupported type: " + type);
@@ -71,115 +67,4 @@ public class DefaultEverythingCollectionService implements EverythingCollectionS
         return dtoConversionService.convertUnknownToResponseDtoReactively(model, conversionConfig);
     }
 
-    private class OwnedCoordinatesHandler implements CoordinatesHandler {
-        @Override
-        public CollectionFragment<Model> fetchCollection(CollectionCoordinates coordinates, Projection projection) {
-            OwnedCoordinates owned = coordinates.getOwnedCoordinates();
-            BasicModel host = retrieveHost(owned);
-
-            Optional<OwnedCollectionFetcher> optFetcher = findFetcher(owned);
-
-            @SuppressWarnings("unchecked")
-            CollectionFragment<Model> castResult = optFetcher
-                    .map(fetcher -> fetcher.fetchCollection(host, projection))
-                    .orElseGet(() -> fetchUsingDefaultConvention(owned, host, projection));
-            return castResult;
-        }
-
-        private BasicModel retrieveHost(OwnedCoordinates owned) {
-            Model host = modelRetriever.retrieveModel(owned.getHostId());
-            if (host == null) {
-                throw createHostNotFoundException(owned);
-            }
-
-            return castToBasicModel(owned, host);
-        }
-
-        private EverythingEverythingException createHostNotFoundException(OwnedCoordinates owned) {
-            String message = format("No host entity was found by external ID '%s'",
-                    owned.getHostId().getExternalId());
-            return new EverythingEverythingException(message);
-        }
-
-        private BasicModel castToBasicModel(OwnedCoordinates owned, Model host) {
-            if (!(host instanceof BasicModel)) {
-                throw new EverythingEverythingException(String.format("Host '%s' is not a BasicModel",
-                        owned.getHostId().getModelType()));
-            }
-
-            return (BasicModel) host;
-        }
-
-        private Optional<OwnedCollectionFetcher> findFetcher(OwnedCoordinates owned) {
-            return ownedCollectionFetchers.stream()
-                            .filter(fetcher -> fetcher.getSupportedModel().equals(owned.getHostId().getModelType()))
-                            .filter(fetcher -> fetcher.getHostAttributeName().equals(owned.getHostAttributeName()))
-                            .findFirst();
-        }
-
-        private CollectionFragment<Model> fetchUsingDefaultConvention(OwnedCoordinates owned,
-                BasicModel host, Projection projection) {
-            FetchByOwnedCoordinates fetcher = new FetchByOwnedCoordinates(universalDao);
-            return fetcher.fetch(host, owned.getHostAttributeName(), projection);
-        }
-
-        @Override
-        public Flux<Model> streamCollection(CollectionCoordinates coordinates, Projection projection) {
-            OwnedCoordinates owned = coordinates.getOwnedCoordinates();
-            if (dbAccessShouldBeMadeInATransaction(owned)) {
-                return streamBlockingFetchResult(coordinates, projection, owned);
-            }
-
-            return streamReactively(projection, owned);
-        }
-
-        private boolean dbAccessShouldBeMadeInATransaction(OwnedCoordinates owned) {
-            return transactivity.isCollectionTransactional(owned.getHostId());
-        }
-
-        private Flux<Model> streamBlockingFetchResult(CollectionCoordinates coordinates, Projection projection,
-                                                      OwnedCoordinates owned) {
-            return reactifier.flux(() -> {
-                return transactivity.doInTransaction(owned.getHostId(), () -> {
-                    return fetchCollection(coordinates, projection).elements();
-                });
-            });
-        }
-
-        private Flux<Model> streamReactively(Projection projection, OwnedCoordinates owned) {
-            Mono<BasicModel> hostMono = retrieveHostReactively(owned);
-
-            Optional<OwnedCollectionStreamer> optStreamer = findStreamer(owned);
-
-            if (optStreamer.isPresent()) {
-                OwnedCollectionStreamer streamer = optStreamer.get();
-                @SuppressWarnings("unchecked")
-                Flux<Model> castModels = hostMono.flatMapMany(host -> streamer.streamCollection(host, projection));
-                return castModels;
-            } else {
-                return fetchUsingDefaultConventionReactively(owned, hostMono, projection);
-            }
-        }
-
-        private Mono<BasicModel> retrieveHostReactively(OwnedCoordinates coordinates) {
-            return modelRetriever.retrieveModelReactively(coordinates.getHostId())
-                    .switchIfEmpty(Mono.error(() -> createHostNotFoundException(coordinates)))
-                    .map(host -> castToBasicModel(coordinates, host));
-        }
-
-        private Optional<OwnedCollectionStreamer> findStreamer(OwnedCoordinates owned) {
-            return ownedCollectionStreamers.stream()
-                    .filter(streamer -> streamer.getSupportedModel().equals(owned.getHostId().getModelType()))
-                    .filter(streamer -> streamer.getHostAttributeName().equals(owned.getHostAttributeName()))
-                    .findFirst();
-        }
-
-        private Flux<Model> fetchUsingDefaultConventionReactively(OwnedCoordinates owned,
-                                                                  Mono<BasicModel> hostMono,
-                                                                  Projection projection) {
-            StreamByOwnedCoordinates streamer = new StreamByOwnedCoordinates(universalDao);
-            return hostMono.flatMapMany(host ->
-                    streamer.stream(host, owned.getHostAttributeName(), projection));
-        }
-    }
 }
