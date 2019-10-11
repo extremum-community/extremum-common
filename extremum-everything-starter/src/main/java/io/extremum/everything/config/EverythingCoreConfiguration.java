6c9d269a18913adb4f3d817aeaf149fbce58573d
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-10-11 21:59:27 +0400

    CS-822 Split everything-everything starter to 2 starters
    - split out evr-core configuration

---
+++ extremum-everything-starter/src/main/java/io/extremum/everything/config/EverythingCoreConfiguration.java
@@ -0,0 +1,208 @@
+package io.extremum.everything.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.extremum.common.collection.conversion.*;
+import io.extremum.common.collection.service.CollectionDescriptorService;
+import io.extremum.common.collection.service.ReactiveCollectionDescriptorService;
+import io.extremum.common.descriptor.service.ReactiveDescriptorService;
+import io.extremum.common.dto.converters.services.DtoConversionService;
+import io.extremum.common.limit.ResponseLimiter;
+import io.extremum.common.limit.ResponseLimiterAdvice;
+import io.extremum.common.limit.ResponseLimiterImpl;
+import io.extremum.common.reactive.Reactifier;
+import io.extremum.common.support.ListBasedUniversalReactiveModelLoaders;
+import io.extremum.common.support.ModelClasses;
+import io.extremum.common.support.UniversalReactiveModelLoader;
+import io.extremum.common.support.UniversalReactiveModelLoaders;
+import io.extremum.common.tx.CollectionTransactivity;
+import io.extremum.common.tx.CollectionTransactor;
+import io.extremum.common.tx.TransactorsCollectionTransactivity;
+import io.extremum.common.urls.ApplicationUrls;
+import io.extremum.common.urls.ApplicationUrlsImpl;
+import io.extremum.everything.aop.ConvertNullDescriptorToModelNotFoundAspect;
+import io.extremum.everything.aop.DefaultEverythingEverythingExceptionHandler;
+import io.extremum.everything.aop.EverythingEverythingExceptionHandler;
+import io.extremum.everything.config.properties.DestroyerProperties;
+import io.extremum.everything.controllers.PingController;
+import io.extremum.everything.dao.SpringDataUniversalDao;
+import io.extremum.everything.dao.UniversalDao;
+import io.extremum.everything.destroyer.EmptyFieldDestroyer;
+import io.extremum.everything.destroyer.EmptyFieldDestroyerConfig;
+import io.extremum.everything.destroyer.PublicEmptyFieldDestroyer;
+import io.extremum.everything.services.*;
+import io.extremum.everything.services.collection.CollectionProviders;
+import io.extremum.everything.services.collection.DefaultEverythingCollectionService;
+import io.extremum.everything.services.collection.EverythingCollectionService;
+import io.extremum.everything.services.collection.ListBasedCollectionProviders;
+import io.extremum.everything.services.defaultservices.DefaultGetter;
+import io.extremum.everything.services.defaultservices.DefaultReactiveGetter;
+import io.extremum.everything.services.management.DefaultEverythingCollectionManagementService;
+import io.extremum.everything.services.management.EverythingCollectionManagementService;
+import io.extremum.everything.services.management.ModelRetriever;
+import io.extremum.everything.services.management.PatcherHooksCollection;
+import io.extremum.sharedmodels.basic.Model;
+import io.extremum.starter.properties.LimitsProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties({DestroyerProperties.class, LimitsProperties.class})
+public class EverythingCoreConfiguration {
+    private final DestroyerProperties destroyerProperties;
+    private final LimitsProperties limitsProperties;
+
+    @Bean
+    @ConditionalOnMissingBean
+    public EmptyFieldDestroyer emptyFieldDestroyer() {
+        EmptyFieldDestroyerConfig config = new EmptyFieldDestroyerConfig();
+        config.setAnalyzablePackagePrefixes(destroyerProperties.getAnalyzablePackagePrefix());
+        return new PublicEmptyFieldDestroyer(config);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(RequestDtoValidator.class)
+    public RequestDtoValidator requestDtoValidator() {
+        return new DefaultRequestDtoValidator();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public PingController pingController() {
+        return new PingController();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(EverythingEverythingExceptionHandler.class)
+    public EverythingEverythingExceptionHandler everythingEverythingExceptionHandler() {
+        return new DefaultEverythingEverythingExceptionHandler();
+    }
+
+    @Bean
+    public ConvertNullDescriptorToModelNotFoundAspect convertNullDescriptorToModelNotFoundAspect() {
+        return new ConvertNullDescriptorToModelNotFoundAspect();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public UniversalDao universalDao(MongoOperations mongoOperations,
+                                     ReactiveMongoOperations reactiveMongoOperations) {
+        return new SpringDataUniversalDao(mongoOperations, reactiveMongoOperations);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public UniversalReactiveModelLoaders universalReactiveModelLoader(List<UniversalReactiveModelLoader> loaders,
+                                                                      ModelClasses modelClasses) {
+        return new ListBasedUniversalReactiveModelLoaders(loaders, modelClasses);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ModelRetriever modelRetriever(List<GetterService<?>> getterServices,
+                                         List<ReactiveGetterService<?>> reactiveGetterServices,
+                                         DefaultGetter defaultGetter,
+                                         DefaultReactiveGetter defaultReactiveGetter) {
+        return new ModelRetriever(getterServices, reactiveGetterServices, defaultGetter, defaultReactiveGetter);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public PatcherHooksCollection patcherHooksCollection(List<PatcherHooksService<?, ?>> patcherHooksServices) {
+        return new PatcherHooksCollection(patcherHooksServices);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CollectionTransactivity collectionTransactivity(List<CollectionTransactor> transactors) {
+        return new TransactorsCollectionTransactivity(transactors);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CollectionProviders collectionProviders(
+            List<OwnedCollectionFetcher> ownedCollectionFetchers,
+            List<OwnedCollectionStreamer> ownedCollectionStreamers,
+            List<FreeCollectionFetcher<? extends Model>> freeCollectionFetchers,
+            List<FreeCollectionStreamer<? extends Model>> freeCollectionStreamers) {
+        return new ListBasedCollectionProviders(ownedCollectionFetchers, ownedCollectionStreamers,
+                freeCollectionFetchers, freeCollectionStreamers);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public EverythingCollectionService everythingCollectionService(
+            ModelRetriever modelRetriever,
+            CollectionProviders collectionProviders,
+            DtoConversionService dtoConversionService,
+            UniversalDao universalDao, Reactifier reactifier,
+            CollectionTransactivity transactivity) {
+        return new DefaultEverythingCollectionService(modelRetriever, collectionProviders,
+                dtoConversionService, universalDao, reactifier, transactivity);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ApplicationUrls applicationUrls() {
+        return new ApplicationUrlsImpl();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CollectionUrls collectionUrls(ApplicationUrls applicationUrls) {
+        return new CollectionUrlsInRoot(applicationUrls);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CollectionMakeup collectionMakeup(CollectionDescriptorService collectionDescriptorService,
+                                             ReactiveCollectionDescriptorService reactiveCollectionDescriptorService,
+                                             CollectionUrls collectionUrls) {
+        return new CollectionMakeupImpl(collectionDescriptorService,
+                reactiveCollectionDescriptorService, collectionUrls);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ResponseCollectionsMakeupAdvice responseCollectionsMakeupAdvice(CollectionMakeup collectionMakeup) {
+        return new ResponseCollectionsMakeupAdvice(collectionMakeup);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ReactiveResponseCollectionsMakeupAspect reactiveResponseCollectionsMakeupAspect(
+            CollectionMakeup collectionMakeup) {
+        return new ReactiveResponseCollectionsMakeupAspect(collectionMakeup);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ResponseLimiter responseLimiter(ObjectMapper objectMapper) {
+        return new ResponseLimiterImpl(limitsProperties.getCollectionTopMaxSizeBytes(), objectMapper);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ResponseLimiterAdvice responseLimiterAdvice(ResponseLimiter limiter) {
+        return new ResponseLimiterAdvice(limiter);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public EverythingCollectionManagementService everythingCollectionManagementService(
+            ReactiveCollectionDescriptorService reactiveCollectionDescriptorService,
+            EverythingCollectionService everythingCollectionService,
+            ReactiveDescriptorService reactiveDescriptorService
+    ) {
+        return new DefaultEverythingCollectionManagementService(
+                reactiveCollectionDescriptorService, everythingCollectionService,
+                reactiveDescriptorService);
+    }
+}
--- extremum-everything-starter/src/main/java/io/extremum/everything/config/EverythingEverythingConfiguration.java
+++ extremum-everything-starter/src/main/java/io/extremum/everything/config/EverythingEverythingConfiguration.java
@@ -3,39 +3,17 @@ package io.extremum.everything.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.extremum.authentication.api.ReactiveSecurityProvider;
 import io.extremum.authentication.api.SecurityProvider;
-import io.extremum.common.collection.conversion.*;
-import io.extremum.common.collection.service.CollectionDescriptorService;
-import io.extremum.common.collection.service.ReactiveCollectionDescriptorService;
 import io.extremum.common.descriptor.service.DescriptorService;
 import io.extremum.common.descriptor.service.ReactiveDescriptorService;
 import io.extremum.common.dto.converters.services.DtoConversionService;
-import io.extremum.common.limit.ResponseLimiter;
-import io.extremum.common.limit.ResponseLimiterAdvice;
-import io.extremum.common.limit.ResponseLimiterImpl;
-import io.extremum.common.reactive.Reactifier;
-import io.extremum.common.support.*;
-import io.extremum.common.tx.CollectionTransactivity;
-import io.extremum.common.tx.CollectionTransactor;
-import io.extremum.common.tx.TransactorsCollectionTransactivity;
-import io.extremum.common.urls.ApplicationUrls;
-import io.extremum.common.urls.ApplicationUrlsImpl;
-import io.extremum.everything.aop.ConvertNullDescriptorToModelNotFoundAspect;
-import io.extremum.everything.aop.DefaultEverythingEverythingExceptionHandler;
-import io.extremum.everything.aop.EverythingEverythingExceptionHandler;
-import io.extremum.everything.config.properties.DestroyerProperties;
+import io.extremum.common.support.CommonServices;
+import io.extremum.common.support.ModelClasses;
+import io.extremum.common.support.ReactiveCommonServices;
+import io.extremum.common.support.UniversalReactiveModelLoaders;
 import io.extremum.everything.controllers.DefaultEverythingEverythingRestController;
 import io.extremum.everything.controllers.EverythingEverythingRestController;
-import io.extremum.everything.controllers.PingController;
-import io.extremum.everything.dao.SpringDataUniversalDao;
-import io.extremum.everything.dao.UniversalDao;
 import io.extremum.everything.destroyer.EmptyFieldDestroyer;
-import io.extremum.everything.destroyer.EmptyFieldDestroyerConfig;
-import io.extremum.everything.destroyer.PublicEmptyFieldDestroyer;
 import io.extremum.everything.services.*;
-import io.extremum.everything.services.collection.CollectionProviders;
-import io.extremum.everything.services.collection.DefaultEverythingCollectionService;
-import io.extremum.everything.services.collection.EverythingCollectionService;
-import io.extremum.everything.services.collection.ListBasedCollectionProviders;
 import io.extremum.everything.services.defaultservices.*;
 import io.extremum.everything.services.management.*;
 import io.extremum.everything.support.DefaultModelDescriptors;
@@ -44,44 +22,22 @@ import io.extremum.everything.support.ModelDescriptors;
 import io.extremum.everything.support.ReactiveModelDescriptors;
 import io.extremum.security.*;
 import io.extremum.security.services.DataAccessChecker;
-import io.extremum.sharedmodels.basic.Model;
 import io.extremum.starter.CommonConfiguration;
-import io.extremum.starter.properties.LimitsProperties;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import org.springframework.context.annotation.Import;
 
 import java.util.List;
 
 @Configuration
-@RequiredArgsConstructor
-@EnableConfigurationProperties({DestroyerProperties.class, LimitsProperties.class})
 @AutoConfigureAfter(CommonConfiguration.class)
 @AutoConfigureBefore(WebMvcAutoConfiguration.class)
+@Import(EverythingCoreConfiguration.class)
 public class EverythingEverythingConfiguration {
-    private final DestroyerProperties destroyerProperties;
-    private final LimitsProperties limitsProperties;
-
-    @Bean
-    @ConditionalOnMissingBean
-    public EmptyFieldDestroyer emptyFieldDestroyer() {
-        EmptyFieldDestroyerConfig config = new EmptyFieldDestroyerConfig();
-        config.setAnalyzablePackagePrefixes(destroyerProperties.getAnalyzablePackagePrefix());
-        return new PublicEmptyFieldDestroyer(config);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(RequestDtoValidator.class)
-    public RequestDtoValidator requestDtoValidator() {
-        return new DefaultRequestDtoValidator();
-    }
 
     @Bean
     @ConditionalOnMissingBean
@@ -113,39 +69,9 @@ public class EverythingEverythingConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public PingController pingController() {
-        return new PingController();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(EverythingEverythingExceptionHandler.class)
-    public EverythingEverythingExceptionHandler everythingEverythingExceptionHandler() {
-        return new DefaultEverythingEverythingExceptionHandler();
-    }
-
-    @Bean
-    public ConvertNullDescriptorToModelNotFoundAspect convertNullDescriptorToModelNotFoundAspect() {
-        return new ConvertNullDescriptorToModelNotFoundAspect();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public UniversalDao universalDao(MongoOperations mongoOperations,
-                                     ReactiveMongoOperations reactiveMongoOperations) {
-        return new SpringDataUniversalDao(mongoOperations, reactiveMongoOperations);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
+    // TODO: move to core?
     public ModelDescriptors modelDescriptors(ModelClasses modelClasses, DescriptorService descriptorService) {
         return new DefaultModelDescriptors(modelClasses, descriptorService);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public UniversalReactiveModelLoaders universalReactiveModelLoader(List<UniversalReactiveModelLoader> loaders,
-                                                                      ModelClasses modelClasses) {
-        return new ListBasedUniversalReactiveModelLoaders(loaders, modelClasses);
     }
 
     @Bean
@@ -176,23 +102,8 @@ public class EverythingEverythingConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public ModelRetriever modelRetriever(List<GetterService<?>> getterServices,
-                                         List<ReactiveGetterService<?>> reactiveGetterServices,
-                                         DefaultGetter defaultGetter,
-                                         DefaultReactiveGetter defaultReactiveGetter) {
-        return new ModelRetriever(getterServices, reactiveGetterServices, defaultGetter, defaultReactiveGetter);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
     public ModelSaver modelSaver(List<SaverService<?>> saverServices, DefaultSaver defaultSaver) {
         return new ModelSaver(saverServices, defaultSaver);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public PatcherHooksCollection patcherHooksCollection(List<PatcherHooksService<?, ?>> patcherHooksServices) {
-        return new PatcherHooksCollection(patcherHooksServices);
     }
 
     @Bean
@@ -351,92 +262,5 @@ public class EverythingEverythingConfiguration {
                 defaultRemover,
                 dtoConversionService, dataSecurity);
         return new RoleSecurityReactiveEverythingManagementService(service, roleSecurity);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public CollectionTransactivity collectionTransactivity(List<CollectionTransactor> transactors) {
-        return new TransactorsCollectionTransactivity(transactors);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public CollectionProviders collectionProviders(
-            List<OwnedCollectionFetcher> ownedCollectionFetchers,
-            List<OwnedCollectionStreamer> ownedCollectionStreamers,
-            List<FreeCollectionFetcher<? extends Model>> freeCollectionFetchers,
-            List<FreeCollectionStreamer<? extends Model>> freeCollectionStreamers) {
-        return new ListBasedCollectionProviders(ownedCollectionFetchers, ownedCollectionStreamers,
-                freeCollectionFetchers, freeCollectionStreamers);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public EverythingCollectionService everythingCollectionService(
-            ModelRetriever modelRetriever,
-            CollectionProviders collectionProviders,
-            DtoConversionService dtoConversionService,
-            UniversalDao universalDao, Reactifier reactifier,
-            CollectionTransactivity transactivity) {
-        return new DefaultEverythingCollectionService(modelRetriever, collectionProviders,
-                dtoConversionService, universalDao, reactifier, transactivity);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public ApplicationUrls applicationUrls() {
-        return new ApplicationUrlsImpl();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public CollectionUrls collectionUrls(ApplicationUrls applicationUrls) {
-        return new CollectionUrlsInRoot(applicationUrls);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public CollectionMakeup collectionMakeup(CollectionDescriptorService collectionDescriptorService,
-                                             ReactiveCollectionDescriptorService reactiveCollectionDescriptorService,
-                                             CollectionUrls collectionUrls) {
-        return new CollectionMakeupImpl(collectionDescriptorService,
-                reactiveCollectionDescriptorService, collectionUrls);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public ResponseCollectionsMakeupAdvice responseCollectionsMakeupAdvice(CollectionMakeup collectionMakeup) {
-        return new ResponseCollectionsMakeupAdvice(collectionMakeup);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public ReactiveResponseCollectionsMakeupAspect reactiveResponseCollectionsMakeupAspect(
-            CollectionMakeup collectionMakeup) {
-        return new ReactiveResponseCollectionsMakeupAspect(collectionMakeup);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public ResponseLimiter responseLimiter(ObjectMapper objectMapper) {
-        return new ResponseLimiterImpl(limitsProperties.getCollectionTopMaxSizeBytes(), objectMapper);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public ResponseLimiterAdvice responseLimiterAdvice(ResponseLimiter limiter) {
-        return new ResponseLimiterAdvice(limiter);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public EverythingCollectionManagementService everythingCollectionManagementService(
-            ReactiveCollectionDescriptorService reactiveCollectionDescriptorService,
-            EverythingCollectionService everythingCollectionService,
-            ReactiveDescriptorService reactiveDescriptorService
-    ) {
-        return new DefaultEverythingCollectionManagementService(
-                reactiveCollectionDescriptorService, everythingCollectionService,
-                reactiveDescriptorService);
     }
 }
