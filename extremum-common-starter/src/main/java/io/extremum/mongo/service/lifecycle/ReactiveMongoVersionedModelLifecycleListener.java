951207679c8c95d2c68647cedcf89c894e968b23
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-10-21 18:01:32 +0400

    CS-856 Add versioned storage prototype for Mongo
    - fill/resolve uuids on versioned mongo models

--- extremum-common-starter/src/main/java/io/extremum/mongo/service/lifecycle/ReactiveMongoVersionedModelLifecycleListener.java
+++ extremum-common-starter/src/main/java/io/extremum/mongo/service/lifecycle/ReactiveMongoVersionedModelLifecycleListener.java
@@ -1,7 +1,7 @@
 package io.extremum.mongo.service.lifecycle;
 
 import io.extremum.mongo.facilities.ReactiveMongoDescriptorFacilities;
-import io.extremum.mongo.model.MongoCommonModel;
+import io.extremum.mongo.model.MongoVersionedModel;
 import io.extremum.mongo.springdata.ReactiveAfterConvertEvent;
 import io.extremum.mongo.springdata.ReactiveAfterSaveEvent;
 import io.extremum.mongo.springdata.ReactiveBeforeConvertEvent;
@@ -11,32 +11,33 @@ import reactor.core.publisher.Mono;
 /**
  * @author rpuch
  */
-public final class ReactiveMongoCommonModelLifecycleListener
-        extends AbstractReactiveMongoEventListener<MongoCommonModel> {
+public final class ReactiveMongoVersionedModelLifecycleListener
+        extends AbstractReactiveMongoEventListener<MongoVersionedModel> {
     private final ReactiveMongoModelsLifecycleSupport modelsLifecycleSupport;
 
-    public ReactiveMongoCommonModelLifecycleListener(ReactiveMongoDescriptorFacilities mongoDescriptorFacilities) {
+    public ReactiveMongoVersionedModelLifecycleListener(ReactiveMongoDescriptorFacilities mongoDescriptorFacilities) {
         modelsLifecycleSupport = new ReactiveMongoModelsLifecycleSupport(mongoDescriptorFacilities);
     }
 
     @Override
-    public Mono<Void> onBeforeConvert(ReactiveBeforeConvertEvent<MongoCommonModel> event) {
-        MongoCommonModel model = event.getSource();
+    public Mono<Void> onBeforeConvert(ReactiveBeforeConvertEvent<MongoVersionedModel> event) {
+        MongoVersionedModel model = event.getSource();
 
         return modelsLifecycleSupport.fillRequiredFields(model);
     }
 
     @Override
-    public Mono<Void> onAfterSave(ReactiveAfterSaveEvent<MongoCommonModel> event) {
-        MongoCommonModel model = event.getSource();
+    public Mono<Void> onAfterSave(ReactiveAfterSaveEvent<MongoVersionedModel> event) {
+        MongoVersionedModel model = event.getSource();
 
         return modelsLifecycleSupport.createDescriptorIfNeeded(model);
     }
 
     @Override
-    public Mono<Void> onAfterConvert(ReactiveAfterConvertEvent<MongoCommonModel> event) {
-        MongoCommonModel model = event.getSource();
+    public Mono<Void> onAfterConvert(ReactiveAfterConvertEvent<MongoVersionedModel> event) {
+        MongoVersionedModel model = event.getSource();
 
         return modelsLifecycleSupport.resolveDescriptor(model);
     }
+
 }
