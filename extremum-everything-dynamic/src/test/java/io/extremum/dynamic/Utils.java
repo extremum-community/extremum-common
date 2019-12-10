2b264f5c910576202dceb8532772f81de0301ce4
Kirill Jonua <k.djonua@gmail.com>
2019-12-10 09:54:20 +0300

    CS-1031 using charset definition in a new Scanner instance in Utils#convertInputStreamToString() method

--- extremum-everything-dynamic/src/test/java/io/extremum/dynamic/Utils.java
+++ extremum-everything-dynamic/src/test/java/io/extremum/dynamic/Utils.java
@@ -3,9 +3,11 @@ package io.extremum.dynamic;
 import java.io.InputStream;
 import java.util.Scanner;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class Utils {
     public static String convertInputStreamToString(InputStream inputStream) {
-        Scanner scanner = new Scanner(inputStream);
+        Scanner scanner = new Scanner(inputStream, UTF_8.name());
         scanner.useDelimiter("\\A");
         return scanner.next();
     }
