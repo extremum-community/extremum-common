afad97436ce84eef43d3e450a80e41ab688e80f6
Kirill Jonua <k.djonua@gmail.com>
2019-12-10 18:38:59 +0300

    CS-1028 Add service for dynamic model instances creation (write-through mode). JsonBasedMongoDynamicModelService with validator & dao interfaces

---
+++ extremum-everything-dynamic/src/main/java/io/extremum/dynamic/models/impl/JsonBasedDynamicModel.java
@@ -0,0 +1,21 @@
+package io.extremum.dynamic.models.impl;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import io.extremum.dynamic.models.DynamicModel;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JsonBasedDynamicModel implements DynamicModel<JsonNode> {
+    private final String modelName;
+    private final JsonNode data;
+
+    @Override
+    public String getModelName() {
+        return modelName;
+    }
+
+    @Override
+    public JsonNode getModelData() {
+        return data;
+    }
+}
