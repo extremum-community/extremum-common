3ce0fe6ff9fc251def4c7e9e77d0913dea615289
Kirill Jonua <k.djonua@gmail.com>
2019-12-10 09:56:21 +0300

    CS-1031 close an InputStream in Utils#convertInputStreamToString() method after resource using

--- extremum-everything-dynamic/src/test/java/io/extremum/dynamic/Utils.java
+++ extremum-everything-dynamic/src/test/java/io/extremum/dynamic/Utils.java
@@ -1,14 +1,21 @@
 package io.extremum.dynamic;
 
+import lombok.SneakyThrows;
+
 import java.io.InputStream;
 import java.util.Scanner;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class Utils {
+    @SneakyThrows
     public static String convertInputStreamToString(InputStream inputStream) {
         Scanner scanner = new Scanner(inputStream, UTF_8.name());
         scanner.useDelimiter("\\A");
-        return scanner.next();
+        try {
+            return scanner.next();
+        } finally {
+            inputStream.close();
+        }
     }
 }
