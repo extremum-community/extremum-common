0cab8947f5e70de5d1f51488c409ba24a7d3d8b5
Kirill Jonua <k.djonua@gmail.com>
2019-12-12 11:18:03 +0300

    CachedNetworkntSchemaProvider - schema loading has become more understandable

--- extremum-everything-dynamic/src/main/java/io/extremum/dynamic/schema/networknt/CachedNetworkntSchemaProvider.java
+++ extremum-everything-dynamic/src/main/java/io/extremum/dynamic/schema/networknt/CachedNetworkntSchemaProvider.java
@@ -5,6 +5,8 @@ import io.extremum.dynamic.validator.exceptions.SchemaNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public abstract class CachedNetworkntSchemaProvider implements NetworkntSchemaProvider {
     private final Map<String, NetworkntSchema> schemas = new HashMap<>();
@@ -12,13 +14,21 @@ public abstract class CachedNetworkntSchemaProvider implements NetworkntSchemaPr
     @Override
     public NetworkntSchema loadSchema(String schemaName) throws SchemaNotFoundException {
         Optional<NetworkntSchema> loaded = loadFromMemory(schemaName);
-        if (loaded.isPresent()) {
-            return loaded.get();
-        } else {
-            Optional<NetworkntSchema> newSchema = getSchema(schemaName);
-            newSchema.ifPresent(schema -> schemas.put(schemaName, schema));
-            return newSchema.orElseThrow(() -> new SchemaNotFoundException(schemaName));
-        }
+
+        return loaded.orElseGet(fetchSchemaFromSource(schemaName));
+    }
+
+    protected Supplier<NetworkntSchema> fetchSchemaFromSource(String schemaName) {
+        return () -> getSchema(schemaName)
+                .map(putToCache(schemaName))
+                .orElseThrow(() -> new SchemaNotFoundException(schemaName));
+    }
+
+    private Function<NetworkntSchema, NetworkntSchema> putToCache(String schemaName) {
+        return s -> {
+            schemas.put(schemaName, s);
+            return s;
+        };
     }
 
     abstract protected Optional<NetworkntSchema> getSchema(String pointer);
