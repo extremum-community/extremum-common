347ca819dc3e46a926cc9e71e52cec7fba215ffe
Kirill Jonua <k.djonua@gmail.com>
2019-12-17 20:39:49 +0300

    CS-1030 GithubResourceLoader renamed to GithubContentsResourceLoader; GithubResourceConfiguration#githubApiBase field is private;

--- extremum-everything-dynamic/src/main/java/io/extremum/dynamic/resources/github/GithubContentsResourceLoader.java
+++ extremum-everything-dynamic/src/main/java/io/extremum/dynamic/resources/github/GithubContentsResourceLoader.java
@@ -7,40 +7,24 @@ import io.extremum.dynamic.resources.UnirestExternalResourceLoader;
 import io.extremum.dynamic.resources.exceptions.ResourceLoadingException;
 import kong.unirest.Config;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpHeaders;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Base64;
 
 @Slf4j
-public class GithubResourceLoader implements ExternalResourceLoader {
-    private final URI baseUri = URI.create("https://api.github.com/");
+public class GithubContentsResourceLoader implements ExternalResourceLoader {
     private final ObjectMapper mapper = new ObjectMapper();
 
     private final ExternalResourceLoader loader;
 
-    public GithubResourceLoader(GithubAccessOptions config) {
+    public GithubContentsResourceLoader(GithubAccessOptions config) {
         Config unirestConfig = new Config();
         unirestConfig.addDefaultHeader(HttpHeaders.AUTHORIZATION, "token " + config.getAuthToken());
         loader = new UnirestExternalResourceLoader(unirestConfig);
-    }
-
-    public String loadContent(String resourceName, GithubResourceConfiguration options) throws ResourceLoadingException {
-        URI uri = makeUri(resourceName, options);
-
-        InputStream is = loadAsInputStream(uri);
-
-        String content = extractContent(is, uri);
-
-        byte[] decodedContent = decodeContent(content);
-
-        return new String(decodedContent);
     }
 
     private String extractContent(InputStream response, URI uri) throws ResourceLoadingException {
@@ -55,26 +39,6 @@ public class GithubResourceLoader implements ExternalResourceLoader {
             log.error(msg, e);
 
             throw new ResourceLoadingException(msg, uri, e);
-        }
-    }
-
-    @NotNull
-    protected URI makeUri(String resourceName, GithubResourceConfiguration opts) {
-        Path path = Paths.get("repos",
-                opts.getOwner(),
-                opts.getRepo()
-        );
-
-        if (opts.getSchemaPath() != null) {
-            path = Paths.get(path.toString(), "contents", opts.getSchemaPath());
-        }
-
-        path = Paths.get(path.toString(), resourceName);
-
-        if (opts.getRef() != null) {
-            return baseUri.resolve(path.toString() + "?ref=" + opts.getRef());
-        } else {
-            return baseUri.resolve(path.toString());
         }
     }
 
