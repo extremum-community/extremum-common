bcc68683ba83471e2474b8eecb058f722365ca4d
Kirill Jonua <k.djonua@gmail.com>
2019-12-17 21:05:58 +0300

    CS-1030 AbstractNetworkntSchemaProvider - some refactoring; SimpleNetworkntSchemaProvider

---
+++ extremum-everything-dynamic/src/main/java/io/extremum/dynamic/schema/provider/networknt/SimpleNetworkntSchemaProvider.java
@@ -0,0 +1,33 @@
+package io.extremum.dynamic.schema.provider.networknt;
+
+import io.extremum.dynamic.resources.NetworkntUrlFetcherExternalResourceLoader;
+import io.extremum.dynamic.resources.ResourceLoader;
+import io.extremum.dynamic.schema.JsonSchemaType;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+
+public class SimpleNetworkntSchemaProvider extends AbstractNetworkntSchemaProvider {
+    private final String basePath;
+
+    public SimpleNetworkntSchemaProvider(JsonSchemaType type, String basePath) {
+        super(type);
+        this.basePath = basePath;
+    }
+
+    @Override
+    protected ResourceLoader getResourceLoader() {
+        return new NetworkntUrlFetcherExternalResourceLoader();
+    }
+
+    @Override
+    protected List<NetworkntURIFetcher> getUriFetchers() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    protected URI makeSchemaUri(String schemaName) {
+        return URI.create(basePath).resolve(schemaName);
+    }
+}
