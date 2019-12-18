955b00adb47fc63a1cc9a59469fc6fcfd9782edf
Kirill Jonua <k.djonua@gmail.com>
2019-12-18 12:24:35 +0300

    CS-1030 SchemaServerExceptionTest, HttpSchemaServerHandlerTest

--- extremum-everything-dynamic/src/main/java/io/extremum/dynamic/server/exceptions/SchemaServerException.java
+++ extremum-everything-dynamic/src/main/java/io/extremum/dynamic/server/exceptions/SchemaServerException.java
@@ -1,9 +1,7 @@
 package io.extremum.dynamic.server.exceptions;
 
-import java.io.IOException;
-
 public class SchemaServerException extends RuntimeException {
-    public SchemaServerException(String message, IOException cause) {
+    public SchemaServerException(String message, Exception cause) {
         super(message, cause);
     }
 
---
+++ extremum-everything-dynamic/src/test/java/io/extremum/dynamic/server/handlers/HttpSchemaServerHandlerTest.java
@@ -0,0 +1,27 @@
+package io.extremum.dynamic.server.handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.nio.file.Path;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+import static org.mockito.Mockito.*;
+
+class HttpSchemaServerHandlerTest {
+    @Test
+    void handlingPlacedHttpExchangeInPassedExecutor() {
+        ExecutorService executur = mock(ExecutorService.class);
+
+        HttpSchemaServerHandler handler = new HttpSchemaServerHandler(executur, mock(Path.class));
+
+        HttpExchange exchange = mock(HttpExchange.class);
+        when(executur.submit(any(Runnable.class))).thenReturn(mock(Future.class));
+
+        handler.handle(exchange);
+
+        Mockito.verify(executur).submit(Mockito.any(Runnable.class));
+    }
+}
