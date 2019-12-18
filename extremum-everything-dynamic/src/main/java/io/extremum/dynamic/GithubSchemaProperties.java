eb89290c5ac5255b3252ef51beb9f3138e84aefc
Kirill Jonua <k.djonua@gmail.com>
2019-12-18 19:32:59 +0300

    CS-1030 GithubSchemaProperties: @ConfigurationProperties("github.schema")

--- extremum-everything-dynamic/src/main/java/io/extremum/dynamic/GithubSchemaProperties.java
+++ extremum-everything-dynamic/src/main/java/io/extremum/dynamic/GithubSchemaProperties.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
-@ConfigurationProperties("github")
+@ConfigurationProperties("github.schema")
 public class GithubSchemaProperties {
     @NotNull
     private int webhookListenerPort;
