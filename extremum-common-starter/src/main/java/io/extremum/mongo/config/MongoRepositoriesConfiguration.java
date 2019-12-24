8d325a8097da69c25a3422e72945837f26368477
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-12-24 11:56:11 +0400

    CS-1123 Improve support for secondary Mongo datasources
    - exclude repositories for secondary datasources on the main datasource

--- extremum-common-starter/src/main/java/io/extremum/mongo/config/MongoRepositoriesConfiguration.java
+++ extremum-common-starter/src/main/java/io/extremum/mongo/config/MongoRepositoriesConfiguration.java
@@ -1,11 +1,13 @@
 package io.extremum.mongo.config;
 
+import io.extremum.common.secondaryds.SecondaryDatasource;
 import io.extremum.mongo.properties.MongoProperties;
 import io.extremum.mongo.springdata.repository.EnableExtremumMongoRepositories;
 import io.extremum.mongo.springdata.reactiverepository.EnableExtremumReactiveMongoRepositories;
 import io.extremum.mongo.springdata.repository.ExtremumMongoRepositoryFactoryBean;
 import io.extremum.mongo.springdata.reactiverepository.ExtremumReactiveMongoRepositoryFactoryBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -13,9 +15,11 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableExtremumMongoRepositories(basePackages = "${mongo.repository-packages}",
-        repositoryFactoryBeanClass = ExtremumMongoRepositoryFactoryBean.class)
+        repositoryFactoryBeanClass = ExtremumMongoRepositoryFactoryBean.class,
+        excludeFilters = @ComponentScan.Filter(SecondaryDatasource.class))
 @EnableExtremumReactiveMongoRepositories(basePackages = "${mongo.repository-packages}",
-        repositoryFactoryBeanClass = ExtremumReactiveMongoRepositoryFactoryBean.class)
+        repositoryFactoryBeanClass = ExtremumReactiveMongoRepositoryFactoryBean.class,
+        excludeFilters = @ComponentScan.Filter(SecondaryDatasource.class))
 @ConditionalOnProperty(MongoProperties.REPOSITORY_PACKAGES_PROPERTY)
 public class MongoRepositoriesConfiguration {
 }
