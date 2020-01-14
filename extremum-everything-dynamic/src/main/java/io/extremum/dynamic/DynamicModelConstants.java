2c109aab2dee6deb693f298a795712709549a48a
Kirill Jonua <k.djonua@gmail.com>
2020-01-14 13:05:48 +0300

    CS-1037 use io.extremum.sharedmodels.constants.DateConstants.DateConstants.FORMAT instead of io.extremum.dynamic.DynamicModelConstants.DATE_FORMAT

--- extremum-everything-dynamic/src/main/java/io/extremum/dynamic/DynamicModelConstants.java
+++ extremum-everything-dynamic/src/main/java/io/extremum/dynamic/DynamicModelConstants.java
@@ -1,7 +1,6 @@
 package io.extremum.dynamic;
 
 public class DynamicModelConstants {
-    public static String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ";
     public static String DATE_FORMAT_PATTERN = "^[\\d]{4}-[\\d]{2}-[\\d]{2}T[\\d]{2}:[\\d]{2}:[\\d]{2}\\.[\\d]{6}-[\\d]{4}";
     public static String CYBERNATED_DATE_TIME_FORMAT_NAME = "cybernated-date-time";
 }
