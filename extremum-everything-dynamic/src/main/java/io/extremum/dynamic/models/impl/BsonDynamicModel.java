5e3df58dded541f6bbbc1a869f4943c39732d30f
Kirill Jonua <k.djonua@gmail.com>
2020-01-15 11:28:58 +0300

    CS-1037 - @AllArgsConstructor, @RequiredArgsConstructor used in BsonDynamicModel

--- extremum-everything-dynamic/src/main/java/io/extremum/dynamic/models/impl/BsonDynamicModel.java
+++ extremum-everything-dynamic/src/main/java/io/extremum/dynamic/models/impl/BsonDynamicModel.java
@@ -2,25 +2,16 @@ package io.extremum.dynamic.models.impl;
 
 import io.extremum.dynamic.models.DynamicModel;
 import io.extremum.sharedmodels.descriptor.Descriptor;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.bson.Document;
 
 @Data
-@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class BsonDynamicModel implements DynamicModel<Document> {
     private Descriptor id;
     private final String modelName;
     private final Document modelData;
-
-    public BsonDynamicModel(String modelName, Document data) {
-        this.modelName = modelName;
-        this.modelData = data;
-    }
-
-    public BsonDynamicModel(Descriptor id, String modelName, Document data) {
-        this.id = id;
-        this.modelName = modelName;
-        this.modelData = data;
-    }
 }
