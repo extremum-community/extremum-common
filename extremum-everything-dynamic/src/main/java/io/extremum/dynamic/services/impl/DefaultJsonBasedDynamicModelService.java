7a3b0d7a5f5d491eccbaf1cba340e0eb96ad274c
Kirill Jonua <k.djonua@gmail.com>
2020-02-14 15:45:20 +0300

    CS-1203 DefaultJsonBasedDynamicModelService fix merge errors

--- extremum-everything-dynamic/src/main/java/io/extremum/dynamic/services/impl/DefaultJsonBasedDynamicModelService.java
+++ extremum-everything-dynamic/src/main/java/io/extremum/dynamic/services/impl/DefaultJsonBasedDynamicModelService.java
@@ -2,7 +2,7 @@ package io.extremum.dynamic.services.impl;
 
 import io.extremum.common.exceptions.ModelNotFoundException;
 import io.extremum.dynamic.dao.DynamicModelDao;
-import io.extremum.dynamic.metadata.impl.DefaultJsonDynamicModelMetadataProvider;
+import io.extremum.dynamic.metadata.MetadataProviderService;
 import io.extremum.dynamic.models.impl.JsonDynamicModel;
 import io.extremum.dynamic.services.DateTypesNormalizer;
 import io.extremum.dynamic.services.DatesProcessor;
@@ -31,7 +31,7 @@ import static reactor.core.publisher.Mono.*;
 public class DefaultJsonBasedDynamicModelService implements JsonBasedDynamicModelService {
     private final DynamicModelDao<JsonDynamicModel> dao;
     private final JsonDynamicModelValidator modelValidator;
-    private final DefaultJsonDynamicModelMetadataProvider metadataProvider;
+    private final MetadataProviderService metadataProvider;
     private final DateTypesNormalizer dateTypesNormalizer;
     private final DatesProcessor datesProcessor;
     private final DynamicModelWatchService watchService;
@@ -108,7 +108,7 @@ public class DefaultJsonBasedDynamicModelService implements JsonBasedDynamicMode
         return getCollectionName(id)
                 .flatMap(cName -> dao.getByIdFromCollection(id, cName))
                 .map(normalize())
-                .map(metadataProvider::provideMetadata)
+                .doOnNext(metadataProvider::provideMetadata)
                 .switchIfEmpty(defer(() -> error(new ModelNotFoundException("DynamicModel with id " + id + " not found"))));
     }
 
