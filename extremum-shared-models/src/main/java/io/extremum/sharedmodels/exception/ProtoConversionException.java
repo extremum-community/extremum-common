7d093571db225ff3295d389c12cd3bbc2959dcff
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2020-02-19 20:56:50 +0400

    CS-1223 Remove spring from shared-models dependencies
    - remove spring
    - separate grpc-related stuff from shared-models

--- extremum-shared-models/src/main/java/io/extremum/sharedmodels/exception/ProtoConversionException.java
+++
@@ -1,7 +0,0 @@
-package io.extremum.sharedmodels.exception;
-
-public class ProtoConversionException extends RuntimeException {
-    public ProtoConversionException(Throwable cause) {
-        super(cause);
-    }
-}
