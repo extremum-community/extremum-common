57c25f4e188bbc290e4409cdfa4e59c117b154c3
Maksim Tyutyaev <maksim.tyutyaev@smekalka.com>
2020-04-17 19:12:25 +0300

    fix/rr-1976: Add tests for MongoDynamicModelDao

---
+++ extremum-everything-dynamic/src/test/java/integration/io/extremum/dynamic/dao/MongoDynamicModelDaoTestConfiguration.java
@@ -0,0 +1,29 @@
+package integration.io.extremum.dynamic.dao;
+
+import io.extremum.authentication.api.SecurityProvider;
+import io.extremum.dynamic.dao.JsonDynamicModelDao;
+import io.extremum.dynamic.dao.MongoDynamicModelDao;
+import io.extremum.dynamic.dao.SoftDeleteRemoveStrategy;
+import io.extremum.mongo.facilities.ReactiveMongoDescriptorFacilities;
+import io.extremum.security.DataSecurity;
+import io.extremum.security.RoleSecurity;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+
+@MockBeans({
+        @MockBean(DataSecurity.class),
+        @MockBean(RoleSecurity.class),
+        @MockBean(SecurityProvider.class)
+})
+@EnableAutoConfiguration
+public class MongoDynamicModelDaoTestConfiguration {
+    @Bean
+    public JsonDynamicModelDao jsonDynamicModelDao(
+            ReactiveMongoOperations ops, ReactiveMongoDescriptorFacilities facilities
+    ) {
+        return new MongoDynamicModelDao(ops, facilities, new SoftDeleteRemoveStrategy(ops));
+    }
+}
